e14c3d93eb41a833fbee3ddc697028cbab9b1ff6
tgfrerer <tim@poniesandlight.co.uk>
2018-02-27 18:21:28 +0000

    cleanup
    + add cpp guards around register_api declaration
    + reformat object declaration body

--- le_renderer/private/le_renderpass.h
+++ le_renderer/private/le_renderpass.h
@@ -7,21 +7,27 @@
 
 #define LE_RENDERPASS_MARKER_EXTERNAL "rp-external"
 
-void register_le_renderpass_api(void *api_);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void register_le_renderpass_api( void *api_ );
+
+#ifdef __cplusplus
+}
+#endif
 
 struct le_renderpass_o {
 
-	uint64_t                        id;
-	uint64_t                        execution_order = 0;
+	uint64_t                                              id;
+	uint64_t                                              execution_order = 0;
 	std::vector<le_renderer_api::image_attachment_info_o> imageAttachments;
-	// std::vector<buffer_attachment_t> bufferAttachments;
 
-	le_renderer_api::pfn_renderpass_setup_t callbackSetup = nullptr;
+	le_renderer_api::pfn_renderpass_setup_t  callbackSetup             = nullptr;
+	le_renderer_api::pfn_renderpass_render_t callbackRender            = nullptr;
+	void *                                   render_callback_user_data = nullptr;
 
-	le_renderer_api::pfn_renderpass_render_t callbackRender = nullptr;
-	void * render_callback_user_data = nullptr;
-
-	char         debugName[ 32 ];
+	char debugName[ 32 ];
 };
 
 #endif //GUARD_LE_RENDERPASS_H
