d2e4332da725722b37b14c7eb7da8679c3f387d6
tgfrerer <tim@poniesandlight.co.uk>
2018-04-04 11:02:25 +0100

    remove image attachment specific resolve,traverse

--- le_renderer/le_rendergraph.cpp
+++ le_renderer/le_rendergraph.cpp
@@ -20,11 +20,10 @@
 // these are some sanity checks for le_renderer_types
 
 #define PRINT_DEBUG_MESSAGES false
+#define LE_GRAPH_BUILDER_RECURSION_DEPTH 20
 
 static_assert(sizeof(le::CommandHeader)==sizeof(uint64_t),"size must be 64bit");
 
-
-#define LE_GRAPH_BUILDER_RECURSION_DEPTH 20
 
 using image_attachment_t = le_renderer_api::image_attachment_info_o;
 
@@ -77,49 +76,7 @@ static void graph_builder_add_renderpass(le_graph_builder_o* self, le_renderpass
 }
 
 // ----------------------------------------------------------------------
-/// \brief find corresponding output for each input attachment,
-/// and tag input with output id
-static void graph_builder_resolve_attachment_ids( std::vector<le_renderpass_o> &passes ) {
-
-	// Rendermodule gives us a pre-sorted list of renderpasses,
-	// we use this to resolve attachment aliases. Since Rendermodule is a linear sequence,
-	// this means that dependencies for resources are well-defined. It's impossible for
-	// two renderpasses using the same resource not to have a clearly defined priority, as
-	// the earliest submitted renderpasses of the two will get priority.
-
-	// map from output id -> source id
-	std::unordered_map<uint64_t, uint64_t, IdentityHash> writeAttachmentTable;
-
-	// We go through passes in module submission order, so that outputs will match later inputs.
-	for ( auto &pass : passes ) {
-
-		// We must first look if any of our READ attachments are already present in the attachment table.
-		// If so, we update source ids (from table) for each attachment we found.
-		for ( auto &attachment : pass.imageAttachments ) {
-			if ( attachment.access_flags & le::AccessFlagBits::eRead ) {
-				auto foundOutputIt = writeAttachmentTable.find( attachment.id );
-				if ( foundOutputIt != writeAttachmentTable.end() ) {
-					attachment.source_id = foundOutputIt->second;
-				}
-			}
-		}
-
-		// Outputs from current pass overwrite any cached outputs with same name:
-		// later inputs with same name will then resolve to the latest version
-		// of an output with a particular name.
-		for ( auto &attachment : pass.imageAttachments ) {
-			if ( attachment.access_flags & le::AccessFlagBits::eWrite ) {
-				writeAttachmentTable[ attachment.id ] = pass.id;
-			}
-		}
-	}
-
-	// TODO: check if all resources have an associated source id, because
-	// resources without source ID are unresolved.
-}
-
-// ----------------------------------------------------------------------
-
+/// \brief find corresponding output for each input resource
 static std::vector<std::vector<uint64_t>> graph_builder_resolve_resource_ids( const std::vector<le_renderpass_o> &passes ) {
 
 	std::vector<std::vector<uint64_t>> dependenciesPerPass;
@@ -172,43 +129,8 @@ static std::vector<std::vector<uint64_t>> graph_builder_resolve_resource_ids( co
 }
 
 // ----------------------------------------------------------------------
-// depth-first traversal of graph, following each input to its corresponding output
-static void graph_builder_traverse_passes( const std::unordered_map<uint64_t, le_renderpass_o *, IdentityHash> &passes,
-                                           const uint64_t &                                                     sourceRenderpassId,
-                                           const uint64_t                                                       recursion_depth ) {
-
-	if ( recursion_depth > LE_GRAPH_BUILDER_RECURSION_DEPTH ) {
-		std::cerr << __FUNCTION__ << " : "
-		          << "max recursion level reached. check for cycles in render graph" << std::endl;
-		return;
-	}
-
-	if (sourceRenderpassId == const_char_hash64(LE_RENDERPASS_MARKER_EXTERNAL)){
-		// TODO: how do we deal with external resources?
-		// std::cerr << __FUNCTION__ << " : READ to attachment referenced which was not previously written in this graph." << std::endl;
-		return;
-	}
-
-	// as each input tells us its source renderpass,
-	// we can look up the provider pass for each source by id
-	auto &sourcePass = passes.at( sourceRenderpassId );
-
-	// We want the maximum edge distance (one recursion equals one edge) from the root node
-	// for each pass, since the max distance makes sure that all resources are available,
-	// even resources which have a shorter path.
-	sourcePass->execution_order = std::max( recursion_depth, sourcePass->execution_order );
-
-	for ( auto &attachment: sourcePass->imageAttachments ) {
-		if (attachment.access_flags & le::AccessFlagBits::eRead){
-			graph_builder_traverse_passes( passes, attachment.source_id, recursion_depth + 1 );
-		}
-	}
-
-}
-
-// ----------------------------------------------------------------------
-
-static void graph_builder_traverse_passes_2( const std::vector<std::vector<uint64_t>> &passes,
+/// \brief depth-first traversal of graph, following each input to its corresponding output
+static void graph_builder_traverse_passes( const std::vector<std::vector<uint64_t>> &passes,
                                              const uint64_t &                          currentRenderpassId,
                                              const uint32_t                            recursion_depth,
                                              std::vector<uint32_t> &                   pass_sort_orders ) {
@@ -231,39 +153,10 @@ static void graph_builder_traverse_passes_2( const std::vector<std::vector<uint6
 	pass_sort_orders[ currentRenderpassId ] = std::max( recursion_depth, pass_sort_orders[ currentRenderpassId ] );
 
 	for ( auto &sourcePass : sourcePasses ) {
-		graph_builder_traverse_passes_2( passes, sourcePass, recursion_depth + 1, pass_sort_orders );
+		graph_builder_traverse_passes( passes, sourcePass, recursion_depth + 1, pass_sort_orders );
 	}
 }
 
-// Re-order renderpasses in topological order, based
-// on graph dependencies
-static void graph_builder_order_passes( std::vector<le_renderpass_o> &passes ) {
-
-	// ----------| invariant: resources know their source id
-
-	std::unordered_map<uint64_t, le_renderpass_o *, IdentityHash> passTable;
-	passTable.reserve( passes.size() );
-
-	for ( auto &p : passes ) {
-		passTable.emplace( p.id, &p );
-	}
-
-	// Note that we set the lowest execution order to 1, so that any passes with order 0
-	// are flagged as non-contributing (these may be pruned)
-	graph_builder_traverse_passes( passTable, const_char_hash64( "root" ), 1 );
-
-	// remove any passes which are not contributing
-	auto endIt = std::remove_if( passes.begin(), passes.end(), []( const le_renderpass_o &pass ) -> bool {
-		return pass.execution_order == 0;
-	} );
-	passes.erase( endIt, passes.end() );
-
-	// sort passes by execution order
-	// Note: we use stable sort so that write-after-write is performed in module submission order.
-	std::stable_sort( passes.begin(), passes.end(), []( const le_renderpass_o &lhs, const le_renderpass_o &rhs ) -> bool {
-		return lhs.execution_order > rhs.execution_order;
-	} );
-}
 
 // ----------------------------------------------------------------------
 
@@ -286,33 +179,44 @@ static std::vector<uint64_t> graph_builder_find_root_passes(const std::vector<le
 
 static void graph_builder_build_graph(le_graph_builder_o* self){
 
-	auto pass_dependencies = graph_builder_resolve_resource_ids(self->passes);
-
-	auto root_passes = graph_builder_find_root_passes(self->passes);
-
-	std::vector<uint32_t> pass_sort_orders; // sort order for each pass in self->passes 
-	pass_sort_orders.resize(self->passes.size(),0);
-
-	for (auto root : root_passes){
-		// note that we begin with sort order 1, so that any passes which have 
-		// sort order 0 still after this loop is complete can be seen as 
-		// marked for deletion / or can be ignored. 
-		graph_builder_traverse_passes_2(pass_dependencies, root, 1, pass_sort_orders);
-	}
-	
 	// Find corresponding output for each input attachment,
 	// and tag input with output id, as dependencies are
 	// declared using names rather than linked in code.
-	graph_builder_resolve_attachment_ids(self->passes);
+	auto pass_dependencies = graph_builder_resolve_resource_ids(self->passes);
 
-	// First, we must establish a toplogical sort order
-	// so that passes which produce resources for other
-	// passes are executed *before* their dependencies
-	//
-	// We use the passes' sort order as a field in the
-	// sorting key for any command buffers associated with that
-	// renderpass.
-	graph_builder_order_passes(self->passes);
+	{
+		// Establish a toplogical sorting order
+		// so that passes which produce resources for other
+		// passes are executed *before* their dependencies
+		//
+		auto root_passes = graph_builder_find_root_passes( self->passes );
+
+		std::vector<uint32_t> pass_sort_orders; // sort order for each pass in self->passes
+		pass_sort_orders.resize( self->passes.size(), 0 );
+
+		for ( auto root : root_passes ) {
+			// note that we begin with sort order 1, so that any passes which have
+			// sort order 0 still after this loop is complete can be seen as
+			// marked for deletion / or can be ignored.
+			graph_builder_traverse_passes( pass_dependencies, root, 1, pass_sort_orders );
+		}
+
+		// We use the passes' sort order as a field in the
+		// sorting key for any command buffers associated with that
+		// renderpass.
+
+		// store sort key with every pass
+		for ( size_t i = 0; i != self->passes.size(); ++i ) {
+			self->passes[ i ].sort_key = pass_sort_orders[ i ];
+		}
+	}
+
+	// Use sort key to order passes in decending order, based on sort key.
+	// pass with lower sort key depends on pass with higher sort key.
+	std::stable_sort(self->passes.begin(),self->passes.end(),[](const le_renderpass_o& lhs, const le_renderpass_o& rhs){
+		return lhs.sort_key > rhs.sort_key;
+	});
+
 }
 
 // ----------------------------------------------------------------------
@@ -334,7 +238,7 @@ static void graph_builder_execute_graph(le_graph_builder_o* self, size_t frameIn
 		msg << "render graph: " << std::endl;
 		for ( const auto &pass : self->passes ) {
 			msg << "renderpass: " << std::setw( 15 ) << std::hex << pass.id << ", "
-			    << "'" << pass.debugName << "' , order: " << pass.execution_order << std::endl;
+			    << "'" << pass.debugName << "' , sort_key: " << pass.sort_key << std::endl;
 
 			for ( const auto &attachment : pass.imageAttachments ) {
 				if ( attachment.access_flags & le::AccessFlagBits::eRead ) {
@@ -362,7 +266,7 @@ static void graph_builder_execute_graph(le_graph_builder_o* self, size_t frameIn
 	auto allocator = backendInterface.get_transient_allocator(backend, frameIndex);
 
 	for (auto & pass: self->passes){
-		if (pass.callbackExecute != nullptr){
+		if (pass.callbackExecute != nullptr && pass.sort_key != 0){
 
 			auto encoder = encoderInterface.create(allocator); // NOTE: we must manually track the lifetime of encoder!
 
@@ -416,17 +320,17 @@ static void render_module_add_renderpass(le_render_module_o*self, le_renderpass_
 
 static void render_module_setup_passes(le_render_module_o* self, le_graph_builder_o* graph_builder_){
 
-	for (auto &pass: self->passes){
+	for ( auto &pass : self->passes ) {
 		// Call setup function on all passes, in order of addition to module
 		//
 		// Setup Function must:
 		// + populate input attachments
 		// + populate output attachments
 		// + (optionally) add renderpass to graph builder.
-		assert(pass.callbackSetup != nullptr);
-		if (pass.callbackSetup(&pass)){
+		assert( pass.callbackSetup != nullptr );
+		if ( pass.callbackSetup( &pass ) ) {
 			// if pass.setup() returns true, this means we shall add this pass to the graph
-			graph_builder_add_renderpass(graph_builder_,&pass);
+			graph_builder_add_renderpass( graph_builder_, &pass );
 		}
 	}
 	// Now, renderpasses should have their attachments properly set.
