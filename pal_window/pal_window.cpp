2d40bed82d44003a1df21f448f541d652e01234f
tgfrerer <tim@poniesandlight.co.uk>
2018-08-16 13:44:22 +0100

    implement debug window placement
    + when debugging, show application window on secondary monitor - this
    keeps the primary monitor free.

--- pal_window/pal_window.cpp
+++ pal_window/pal_window.cpp
@@ -344,9 +344,29 @@ static pal_window_o *window_create( const pal_window_settings_o *settings_ ) {
 	// TODO: implement GLFW window hints, based on settings.
 	// See: http://www.glfw.org/docs/latest/window_guide.html#window_hints
 	glfwWindowHint( GLFW_FLOATING, GLFW_TRUE ); // < window is created so that it is always on top.
+	glfwWindowHint( GLFW_VISIBLE, GLFW_FALSE ); // < window is initially not visible
 	glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
 
 	obj->window = glfwCreateWindow( obj->mSettings.width, obj->mSettings.height, obj->mSettings.title.c_str(), obj->mSettings.monitor, nullptr );
+
+#ifndef NDEBUG
+
+	int           monitorCount = 0;
+	GLFWmonitor **monitors     = glfwGetMonitors( &monitorCount );
+
+	int windowX = 100;
+	int windowY = 100;
+
+	if ( monitorCount > 1 ) {
+		// if there is more than one monitor, we want our window to appear on the secondary monitor by default.
+		glfwGetMonitorPos( monitors[ 1 ], &windowX, &windowY );
+	}
+
+	glfwSetWindowPos( obj->window, windowX, windowY );
+
+#endif
+
+	glfwShowWindow( obj->window );
 
 	// Set the user pointer so callbacks know which window they belong to
 	glfwSetWindowUserPointer( obj->window, obj );
