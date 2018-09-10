d52254d57e4ceb2c3ff36a2b99bb386a9f5f4633
tgfrerer <tim@poniesandlight.co.uk>
2018-09-10 10:38:07 +0100

    update formatting

--- test_app/test_app.h
+++ test_app/test_app.h
@@ -15,29 +15,31 @@ void register_test_app_api( void *api );
 
 struct test_app_o;
 
+// clang-format off
 struct test_app_api {
 
 	static constexpr auto id      = "test_app";
 	static constexpr auto pRegFun = register_test_app_api;
 
 	struct test_app_interface_t {
-		test_app_o *( *create )();
-		void ( *destroy )( test_app_o *self );
-		bool ( *update )( test_app_o *self );
+		test_app_o * ( *create                   )();
+		void         ( *destroy                  )( test_app_o *self );
+		bool         ( *update                   )( test_app_o *self );
 
-		void ( *key_callback )( void *user_data, int key, int scancode, int action, int mods );
-		void ( *character_callback )( void *user_data, unsigned int codepoint );
-		void ( *cursor_position_callback )( void *user_data, double xpos, double ypos );
-		void ( *cursor_enter_callback )( void *user_data, int entered );
-		void ( *mouse_button_callback )( void *user_data, int button, int action, int mods );
-		void ( *scroll_callback )( void *user_data, double xoffset, double yoffset );
+		void         ( *key_callback             )( void *user_data, int key, int scancode, int action, int mods );
+		void         ( *character_callback       )( void *user_data, unsigned int codepoint );
+		void         ( *cursor_position_callback )( void *user_data, double xpos, double ypos );
+		void         ( *cursor_enter_callback    )( void *user_data, int entered );
+		void         ( *mouse_button_callback    )( void *user_data, int button, int action, int mods );
+		void         ( *scroll_callback          )( void *user_data, double xoffset, double yoffset );
 
-		void ( *initialize )(); // static methods
-		void ( *terminate )();  // static methods
+		void         ( *initialize               )(); // static methods
+		void         ( *terminate                )(); // static methods
 	};
 
 	test_app_interface_t test_app_i;
 };
+// clang-format on
 
 #ifdef __cplusplus
 
