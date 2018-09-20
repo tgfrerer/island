dba08b81bf00539af8d6c7c8abb2de0a756a087f
tgfrerer <tim@poniesandlight.co.uk>
2018-09-20 15:49:20 +0100

    create dependency_manager module
    + refactor simple_module to only do tests, create freestanding module
    for dependency manager

---
+++ le_dependency_manager/le_dependency_manager.cpp
@@ -0,0 +1,343 @@
+#include "le_dependency_manager.h"
+#include "pal_api_loader/ApiRegistry.hpp"
+
+#include "iostream"
+#include "iomanip"
+#include <bitset>
+#include <vector>
+#include <assert.h>
+
+constexpr size_t MAX_NUM_LAYER_RESOURCES                = 64;
+using BitField                                          = std::bitset<MAX_NUM_LAYER_RESOURCES>;
+constexpr uint64_t le_dependency_manager_ROOT_LAYER_TAG = hash_32_fnv1a_const( "DEPENDENCY_MANAGER_ROOT_LAYER_TAG" );
+
+enum AccessType : int {
+	eAccessTypeRead      = 0b01,
+	eAccessTypeWrite     = 0b10,
+	eAccessTypeReadWrite = eAccessTypeRead | eAccessTypeWrite,
+};
+
+struct Layer {
+	BitField reads;
+	BitField writes;
+};
+
+/// When we add a Resource to the dependency tracker,
+///
+/// 0. we first must know the current layer
+/// 1. we compare the resource id against resources already known to the tracker
+/// 2. if resource is known, we use the index into the ids vector to identify the resource
+/// 3. we update the current layer at the index of the current resource based on the resource's read or write access.
+///
+/// This means there is a lookup into a vector of resources for every resource which we add to the layer - this vector may grow, so we always just store indices.
+///
+struct le_dependency_manager_o {
+	std::array<uint64_t, MAX_NUM_LAYER_RESOURCES> knownResources; // Stores all known resources over all layers, provides us with canonical indices for each known resource
+	size_t                                        knownResourcesCount = 0;
+	std::vector<Layer>                            layers;             // r/w information for each layer. `Layer::reads` and `Layer::writes` bitfield indices correspond to `knownResources` indices
+	std::vector<uint32_t>                         layers_sort_order;  // Sort order for layers
+	std::vector<std::string>                      layers_debug_names; // debug name for each layer(optional), but must be same element count as layers.
+	std::vector<uint8_t>                          layers_flags;       // bitmask representing whether a layer is a root layer (root layers and their contributors must always be processed), and whether a layer must be processed as it does contribute.
+};
+
+/*
+ * I think - it should be possible to get any dependent layers by up-walking the layers array
+ * and looking for writes where we have reads in a later layer.
+ *
+ * Our goal is to discard any layers which have no contribution to the final product.
+ *
+ * 1. First eliminate any layers which have no effect on any root nodes
+ * -- we go from last root layer to first layer .
+ * -- we accumulate reads
+ * -- if there is a write in l(n-1) where we have a read in l(accum) then l(n-1) is a provider.
+ * -- null out all positions in accum where there was a write in l(n-1), unless there was a read-write, in which case we keep the read
+ *
+ * 2. Calculate sort indices for layers.
+ *
+ 0 00
+ * 01 (used by 2)
+ *
+ 1 00 (can be discarded)
+ * 00
+ *
+ 2 01
+ * 01 (used by 2, 3)
+ *
+ 3 01
+ * 10
+ *
+ *
+ * */
+
+/// tag any layers which are not root - or which provably don't contribute to any root layer
+static void layers_tag_contributing( Layer *const layers, const size_t numLayers ) {
+
+	// we must iterate backwards from last layer to first layer
+	Layer *            layer      = layers + numLayers;
+	Layer const *const layer_rend = layers;
+
+	BitField read_accum;
+
+	// find first root layer
+	//    monitored reads will be from the first root layer
+
+	while ( layer != layer_rend ) {
+		--layer;
+
+		bool isRoot = layer->reads[ 0 ]; // any layer which has the root signal set in the first read channel is considered a root layer
+
+		// If it's a root layer, get all reads from this layer
+		// If it's not a root layer, see first if there are any writes to currently monitored reads
+		//    if yes, add all reads to monitored reads
+
+		if ( isRoot || ( layer->writes & read_accum ).any() ) {
+			// If this layer is a root layer - OR					   ) this means the layer is contributing
+			// If this layer writes to any subsequent monitored reads, )
+			// Then we must monitor all reads of this layer.
+			read_accum |= layer->reads;
+
+			layer->reads[ 0 ] = true; // Make sure the layer is tagged as contributing
+		} else {
+			// Otherwise - this layer does not contribute
+		}
+	} // end for all layers, backwards iteration
+}
+
+// ----------------------------------------------------------------------
+
+/// Note: `sortIndices` must point to an array of `numLayers` elements of type uint32_t
+static void layers_calculate_sort_indices( Layer const *const layers, const size_t numLayers, uint32_t *sortIndices ) {
+
+	BitField read_accum{};
+	BitField write_accum{};
+
+	/// Each bit in the layer bitfield stands for one resource.
+	/// Bitfield index corresponds to a resource id. Note that
+	/// bitfields are indexed right-to left (index zero is right-most).
+
+	bool needs_barrier = false;
+
+	uint32_t sortIndex = 0;
+
+	{
+		Layer const *const layers_end = layers + numLayers;
+		uint32_t *         layerO     = sortIndices;
+		for ( Layer const *layer = layers; layer != layers_end; layer++, layerO++ ) {
+
+			// Weed out any layers which are marked as non-contributing
+
+			if ( layer->reads[ 0 ] == false ) {
+				*layerO = -1; // tag layer as not contributing
+				continue;
+			}
+
+			BitField read_write = ( layer->reads & layer->writes ); // read_after write in same layer - this means a layer boundary if it does touch any previously read or written elements
+
+			// A barrier is needed, if:
+			needs_barrier = ( read_accum & read_write ).any() ||     // - any previously read elements are touched by read-write, OR
+			                ( write_accum & read_write ).any() ||    // - any previously written elements are touched by read-write, OR
+			                ( write_accum & layer->reads ).any() ||  // - the current layer wants to read from a previously written layer, OR
+			                ( write_accum & layer->writes ).any() || // - the current layer writes to a previously written resource, OR
+			                ( read_accum & layer->writes ).any();    // - the current layer wants to write to a layer which was previously read.
+
+			// print out debug information
+			std::cout << "Needs barrier: " << ( needs_barrier ? "true" : "false" ) << std::endl
+			          << std::flush;
+
+			if ( needs_barrier ) {
+				++sortIndex;         // Barriers are expressed by increasing the sortIndex. Layers with the same sortIndex *may* execute concurrently.
+				read_accum.reset();  // Barriers apply everything before the current layer
+				write_accum.reset(); //
+				needs_barrier = false;
+			}
+
+			write_accum |= layer->writes;
+			read_accum |= layer->reads;
+
+			*layerO = sortIndex;
+		}
+	}
+
+	// print out debug information
+	//	std::cout << "Needs barrier: " << ( needs_barrier ? "true" : "false" ) << std::endl
+	//	          << std::flush;
+
+	//	std::cout << layer->reads << " reads" << std::endl
+	//	          << std::flush;
+	//	std::cout << read_accum << " read accum" << std::endl
+	//	          << std::flush;
+	//	std::cout << write_accum << " write accum" << std::endl
+	//	          << std::flush;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_reset( le_dependency_manager_o *self ) {
+
+	// NOTE: we don't have to explicitly reset the contents of self->knownResources, since self->knownResourcesCount
+	// keeps track of the number of valid elements in the array. We only make sure that the very first element in
+	// knownResources is a special tag which we use to tag a layer as a root (i.e. a layer which always contributes)
+	// layer.
+	self->knownResourcesCount = 1;
+	self->knownResources[ 0 ] = le_dependency_manager_ROOT_LAYER_TAG;
+
+	self->layers.clear();
+	self->layers_sort_order.clear();
+	self->layers_debug_names.clear();
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_add_resource( le_dependency_manager_o *self, uint64_t resourceID, uint8_t access_type ) {
+	// - Check if resource is in known resources
+	// - If not, add to known resources
+
+	size_t       resource_idx      = 0;
+	const size_t numKnownResources = self->knownResourcesCount;
+	for ( uint64_t *currentRes = self->knownResources.data(); resource_idx != numKnownResources; ++resource_idx, ++currentRes ) {
+		if ( *currentRes == resourceID ) {
+			break;
+		}
+	}
+
+	if ( resource_idx == numKnownResources ) {
+		// resourceID was not in knownResources; we must add it
+
+		// we cannot track more individual resources than we have channels in our bitset.
+		assert( self->knownResourcesCount < MAX_NUM_LAYER_RESOURCES );
+
+		self->knownResources[ numKnownResources ] = resourceID;
+		self->knownResourcesCount++;
+	}
+
+	// ----------| invariant: resource_idx names the resource in question
+
+	// -- ensure there is a current layer
+
+	if ( self->layers.empty() ) {
+		self->layers.emplace_back(); // add an empty layer
+		self->layers_debug_names.emplace_back( "default" );
+	}
+
+	// -- fetch current layer
+
+	auto &currentLayer = self->layers.back();
+
+	// -- add resource to current layer
+
+	if ( access_type & eAccessTypeRead ) {
+		currentLayer.reads |= ( 1 << resource_idx );
+	}
+	if ( access_type & eAccessTypeWrite ) {
+		currentLayer.writes |= ( 1 << resource_idx );
+	}
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_next_layer( le_dependency_manager_o *self, char const *debug_name = nullptr ) {
+	self->layers.emplace_back();
+	self->layers_debug_names.emplace_back( debug_name );
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_next_root_layer( le_dependency_manager_o *self, char const *debug_name = nullptr ) {
+	self->layers.emplace_back();
+	le_dependency_manager_add_resource( self, le_dependency_manager_ROOT_LAYER_TAG, eAccessTypeRead );
+	self->layers_debug_names.emplace_back( debug_name );
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_print_sort_order( le_dependency_manager_o *self ) {
+	for ( size_t i = 0; i != self->layers_sort_order.size(); ++i ) {
+		std::cout << "Layer " << std::dec << std::setw( 3 ) << i << " sort order : " << std::setw( 3 ) << self->layers_sort_order[ i ] << " - (`" << self->layers_debug_names[ i ] << "`)" << std::endl
+		          << std::flush;
+	}
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_resolve_dependencies( le_dependency_manager_o *self ) {
+
+	self->layers_sort_order.resize( self->layers.size(), 0 );
+
+	layers_tag_contributing( self->layers.data(), self->layers.size() );
+
+	layers_calculate_sort_indices( self->layers.data(), self->layers.size(), self->layers_sort_order.data() );
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_get_layer_sort_indices( le_dependency_manager_o *self, uint32_t **pIndices, size_t *pIndicesCount ) {
+	*pIndices      = self->layers_sort_order.data();
+	*pIndicesCount = self->layers_sort_order.size();
+};
+
+// ----------------------------------------------------------------------
+
+static le_dependency_manager_o *le_dependency_manager_create() {
+	auto self = new le_dependency_manager_o();
+	le_dependency_manager_reset( self );
+	return self;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_dependency_manager_destroy( le_dependency_manager_o *self ) {
+	delete self;
+}
+
+// ----------------------------------------------------------------------
+
+static bool test_sorting() {
+	std::vector<Layer> layers = {
+	    {
+	        0b000001, // reads
+	        0b011001, // writes
+	    },
+	    {
+	        0b010010, // reads
+	        0b000110, // writes
+	    },
+	    {
+	        0b001100, // reads
+	        0b000100, // writes
+	    },
+	    {
+	        0b111011, // reads
+	        0b100001, // writes
+	    },
+	    {
+	        0b001000, // reads
+	        0b000100, // writes
+	    },
+	};
+
+	std::vector<uint32_t> layerOrders( layers.size() ); // Holds one sort index for each layer
+
+	layers_calculate_sort_indices( layers.data(), layers.size(), layerOrders.data() );
+
+	for ( size_t i = 0; i != layerOrders.size(); ++i ) {
+		std::cout << "Layer " << std::dec << i << ", sort order : " << layerOrders[ i ] << std::endl
+		          << std::flush;
+	}
+
+	return true;
+}
+
+// ----------------------------------------------------------------------
+
+ISL_API_ATTR void register_le_dependency_manager_api( void *api ) {
+	auto &le_dependency_manager_i = static_cast<le_dependency_manager_api *>( api )->le_dependency_manager_i;
+
+	le_dependency_manager_i.create                 = le_dependency_manager_create;
+	le_dependency_manager_i.destroy                = le_dependency_manager_destroy;
+	le_dependency_manager_i.add_resource           = le_dependency_manager_add_resource;
+	le_dependency_manager_i.next_layer             = le_dependency_manager_next_layer;
+	le_dependency_manager_i.next_root_layer        = le_dependency_manager_next_root_layer;
+	le_dependency_manager_i.resolve_dependencies   = le_dependency_manager_resolve_dependencies;
+	le_dependency_manager_i.get_layer_sort_indices = le_dependency_manager_get_layer_sort_indices;
+	le_dependency_manager_i.print_sort_order       = le_dependency_manager_print_sort_order;
+}
--- simple_module/simple_module.cpp
+++ simple_module/simple_module.cpp
@@ -1,29 +1,16 @@
 #include "simple_module.h"
 #include "pal_api_loader/ApiRegistry.hpp"
 
+#include "le_dependency_manager/le_dependency_manager.h"
+
 #include "iostream"
 #include "iomanip"
 #include <chrono>
 #include <thread>
-#include <bitset>
 #include <vector>
-#include <assert.h>
-
-constexpr size_t MAX_NUM_LAYER_RESOURCES = 64;
-using BitField                           = std::bitset<MAX_NUM_LAYER_RESOURCES>;
-
-enum AccessType : int {
-	eAccessTypeRead      = 0b01,
-	eAccessTypeWrite     = 0b10,
-	eAccessTypeReadWrite = eAccessTypeRead | eAccessTypeWrite,
-};
-
-struct Layer {
-	BitField reads;
-	BitField writes;
-};
 
 struct simple_module_o {
+	LeDependencyManager manager;
 };
 
 // ----------------------------------------------------------------------
@@ -41,260 +28,43 @@ static void simple_module_destroy( simple_module_o *self ) {
 
 // ----------------------------------------------------------------------
 
-/// Note: `sortIndices` must point to an array of `numLayers` elements of type uint32_t
-static void layers_calculate_sort_indices( Layer const *const layers, const size_t numLayers, uint32_t *sortIndices ) {
-
-	BitField read_accum{};
-	BitField write_accum{};
-
-	/// Each bit in the layer bitfield stands for one resource.
-	/// Bitfield index corresponds to a resource id. Note that
-	/// bitfields are indexed right-to left (index zero is right-most).
-
-	bool needs_barrier = false;
-
-	uint32_t sortIndex = 0;
-
-	{
-		Layer const *const layers_end = layers + numLayers;
-		uint32_t *         layerO     = sortIndices;
-		for ( Layer const *layer = layers; layer != layers_end; layer++, layerO++ ) {
-
-			BitField read_write = ( layer->reads & layer->writes ); // read_after write in same layer - this means a layer boundary if it does touch any previously read or written elements
-
-			// A barrier is needed, if:
-			needs_barrier = ( read_accum & read_write ).any() ||     // - any previously read elements are touched by read-write, OR
-			                ( write_accum & read_write ).any() ||    // - any previously written elements are touched by read-write, OR
-			                ( write_accum & layer->reads ).any() ||  // - the current layer wants to read from a previously written layer, OR
-			                ( write_accum & layer->writes ).any() || // - the current layer writes to a previously written resource, OR
-			                ( read_accum & layer->writes ).any();    // - the current layer wants to write to a layer which was previously read.
-
-			// print out debug information
-			std::cout << "Needs barrier: " << ( needs_barrier ? "true" : "false" ) << std::endl
-			          << std::flush;
-
-			if ( needs_barrier ) {
-				++sortIndex;         // Barriers are expressed by increasing the sortIndex. Layers with the same sortIndex *may* execute concurrently.
-				read_accum.reset();  // Barriers apply everything before the current layer
-				write_accum.reset(); //
-				needs_barrier = false;
-			}
-
-			write_accum |= layer->writes;
-			read_accum |= layer->reads;
-
-			*layerO = sortIndex;
-		}
-	}
-
-	// print out debug information
-	//	std::cout << "Needs barrier: " << ( needs_barrier ? "true" : "false" ) << std::endl
-	//	          << std::flush;
-
-	//	std::cout << layer->reads << " reads" << std::endl
-	//	          << std::flush;
-	//	std::cout << read_accum << " read accum" << std::endl
-	//	          << std::flush;
-	//	std::cout << write_accum << " write accum" << std::endl
-	//	          << std::flush;
-}
-
-/// When we add a Resource to the dependency tracker,
-///
-/// 0. we first must know the current layer
-/// 1. we compare the resource id against resources already known to the tracker
-/// 2. if resource is known, we use the index into the ids vector to identify the resource
-/// 3. we update the current layer at the index of the current resource based on the resource's read or write access.
-///
-/// This means there is a lookup into a vector of resources for every resource which we add to the layer - this vector may grow, so we always just store indices.
-///
-struct dependency_manager_o {
-	std::array<uint64_t, MAX_NUM_LAYER_RESOURCES> knownResources; // Stores all known resources over all layers, provides us with canonical indices for each known resource
-	size_t                                        knownResourcesCount = 0;
-	std::vector<Layer>                            layers;             // r/w information for each layer. `Layer::reads` and `Layer::writes` bitfield indices correspond to `knownResources` indices
-	std::vector<uint32_t>                         layers_sort_order;  // Sort order for layers
-	std::vector<std::string>                      layers_debug_names; // debug name for each layer(optional), but must be same element count as layers.
-};
-
-static dependency_manager_o *dependency_manager_create() {
-	auto self = new dependency_manager_o();
-	return self;
-}
-
-static void dependency_manager_destroy( dependency_manager_o *self ) {
-	delete self;
-}
-
-static void dependency_manager_reset( dependency_manager_o *self ) {
-
-	// NOTE: we don't have to explicitly reset the contents of self->knownResources, since self->knownResourcesCount
-	// keeps track of the number of valid elements in the array.
-	self->knownResourcesCount = 0;
-	self->layers.clear();
-	self->layers_sort_order.clear();
-	self->layers_debug_names.clear();
-}
-
-static void dependency_manager_add_resource( dependency_manager_o *self, uint64_t resourceID, uint8_t access_type ) {
-	// - Check if resource is in known resources
-	// - If not, add to known resources
-
-	size_t       resource_idx      = 0;
-	const size_t numKnownResources = self->knownResourcesCount;
-	for ( uint64_t *currentRes = self->knownResources.data(); resource_idx != numKnownResources; ++resource_idx, ++currentRes ) {
-		if ( *currentRes == resourceID ) {
-			break;
-		}
-	}
-
-	if ( resource_idx == numKnownResources ) {
-		// resourceID was not in knownResources; we must add it
-
-		// we cannot track more individual resources than we have channels in our bitset.
-		assert( self->knownResourcesCount < MAX_NUM_LAYER_RESOURCES );
-
-		self->knownResources[ numKnownResources ] = resourceID;
-		self->knownResourcesCount++;
-	}
-
-	// ----------| invariant: resource_idx names the resource in question
-
-	// -- ensure there is a current layer
-
-	if ( self->layers.empty() ) {
-		self->layers.emplace_back(); // add an empty layer
-		self->layers_debug_names.emplace_back( "default" );
-	}
-
-	// -- fetch current layer
-
-	auto &currentLayer = self->layers.back();
-
-	// -- add resource to current layer
-
-	if ( access_type & eAccessTypeRead ) {
-		currentLayer.reads |= ( 1 << resource_idx );
-	}
-	if ( access_type & eAccessTypeWrite ) {
-		currentLayer.writes |= ( 1 << resource_idx );
-	}
-}
-
-// ----------------------------------------------------------------------
-
-static void dependency_manager_next_layer( dependency_manager_o *self, char const *debug_name = nullptr ) {
-	self->layers.emplace_back();
-	self->layers_debug_names.emplace_back( debug_name );
-}
-
-// ----------------------------------------------------------------------
-
-static void dependency_manager_print_sort_order( dependency_manager_o *self ) {
-	for ( size_t i = 0; i != self->layers_sort_order.size(); ++i ) {
-		std::cout << "Layer " << std::dec << std::setw( 3 ) << i << " sort order : " << std::setw( 3 ) << self->layers_sort_order[ i ] << " - (`" << self->layers_debug_names[ i ] << "`)" << std::endl
-		          << std::flush;
-	}
-}
-
-// ----------------------------------------------------------------------
-
-static void dependency_manager_resolve_dependencies( dependency_manager_o *self ) {
-
-	self->layers_sort_order.resize( self->layers.size(), 0 );
-
-	layers_calculate_sort_indices( self->layers.data(), self->layers.size(), self->layers_sort_order.data() );
-}
-
-// ----------------------------------------------------------------------
-
 static bool test_dependency_manager() {
-	auto dm = dependency_manager_create();
+	auto dm = LeDependencyManager();
 
-	dependency_manager_next_layer( dm, "layer 0" );
-	dependency_manager_add_resource( dm, 33, eAccessTypeReadWrite );
-	dependency_manager_add_resource( dm, 12, eAccessTypeRead );
-	dependency_manager_next_layer( dm, "layer 1" );
-	dependency_manager_add_resource( dm, 1, eAccessTypeWrite );
-	dependency_manager_add_resource( dm, 2, eAccessTypeWrite );
-	dependency_manager_next_layer( dm, "layer 2" );
-	dependency_manager_add_resource( dm, 3, eAccessTypeReadWrite );
-	dependency_manager_add_resource( dm, 2, eAccessTypeRead );
+	using namespace le_dependency_manager;
 
-	dependency_manager_resolve_dependencies( dm );
+	dm.nextLayer( "layer initial" );
+	dm.addResource( 2, eAccessTypeRead );
+	dm.addResource( 33, eAccessTypeWrite );
 
-	dependency_manager_print_sort_order( dm );
+	dm.nextLayer( "layer 0", true );
+	dm.addResource( 33, eAccessTypeRead );
+	dm.addResource( 12, eAccessTypeWrite );
 
-	dependency_manager_destroy( dm );
+	dm.nextLayer( "layer 1" );
+	dm.addResource( 2, eAccessTypeReadWrite );
+	dm.addResource( 1, eAccessTypeWrite );
 
-	return true;
-}
+	dm.nextLayer( "layer 2", true );
+	dm.addResource( 2, eAccessTypeWrite );
+	dm.addResource( 3, eAccessTypeReadWrite );
 
-// ----------------------------------------------------------------------
+	dm.resolveDependencies();
+	dm.printSortOrder();
 
-static bool test_sorting() {
-	std::vector<Layer> layers = {
-	    {
-	        0b000001, // reads
-	        0b011001, // writes
-	    },
-	    {
-	        0b010010, // reads
-	        0b000110, // writes
-	    },
-	    {
-	        0b001100, // reads
-	        0b000100, // writes
-	    },
-	    {
-	        0b111011, // reads
-	        0b100001, // writes
-	    },
-	    {
-	        0b001000, // reads
-	        0b000100, // writes
-	    },
-	};
+	uint32_t *sort_start;
+	size_t    sort_count;
+	dm.getLayerSortIndices( &sort_start, &sort_count );
+	std::vector<uint32_t> sort_order{sort_start, sort_start + sort_count};
 
-	std::vector<uint32_t> layerOrders( layers.size() ); // Holds one sort index for each layer
+	for ( size_t i = 0; i != sort_order.size(); ++i ) {
 
-	layers_calculate_sort_indices( layers.data(), layers.size(), layerOrders.data() );
-
-	for ( size_t i = 0; i != layerOrders.size(); ++i ) {
-		std::cout << "Layer " << std::dec << i << ", sort order : " << layerOrders[ i ] << std::endl
+		std::cout << "layer #" << i << " : " << sort_order[ i ] << std::endl
 		          << std::flush;
 	}
 
 	return true;
 }
-
-/*
- * I think - it should be possible to get any dependent layers by up-walking the layers array
- * and looking for writes where we have reads in a later layer.
- *
- * Our goal is to discard any layers which have no contribution to the final product.
- *
- * 1. First eliminate any layers which have no effect on any root nodes
- * -- we go from last root layer to first layer .
- * -- we accumulate reads
- * -- if there is a write in l(n-1) where we have a read in l(accum) then l(n-1) is a provider.
- * -- null out all positions in accum where there was a write in l(n-1), unless there was a read-write, in which case we keep the read
- *
- * 2. Calculate sort indices for layers.
- *
- 0 00
- * 01 (used by 2)
- *
- 1 00 (can be discarded)
- * 00
- *
- 2 01
- * 01 (used by 2, 3)
- *
- 3 01
- * 10
- *
- *
- * */
 
 // ----------------------------------------------------------------------
 
@@ -315,8 +85,6 @@ static bool run_tests() {
 		          << std::flush;
 		result &= testResult;
 	}
-
-	test_sorting();
 
 	test_dependency_manager();
 
@@ -339,7 +107,7 @@ static void simple_module_update( simple_module_o *self ) {
 		firstRun = false;
 	} else {
 		using std::chrono_literals::operator""ms;
-		std::this_thread::sleep_for( 100ms );
+		// std::this_thread::sleep_for( 100ms );
 	}
 }
 
