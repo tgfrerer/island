a5fbe081f87bf35eb93157dadccdc85eb1944659
tgfrerer <tim@poniesandlight.co.uk>
2018-10-30 11:24:12 +0000

    update shaders

--- resources/shaders/include_circle.glsl
+++ resources/shaders/include_circle.glsl
@@ -1,15 +1,15 @@
 #ifndef INCLUDE_CIRCLE_GLSL
 #define INCLUDE_CIRCLE_GLSL
 
-vec4 getCircle(in vec2 t){
+float getCircle(in vec2 t, float circle_radius){
 
 	float dist = length(t - vec2(0.5)); // distance will be at max 0.5
 	dist *= 2; // scale dist to 0..1 range
-	float circle_smoothness = 0.2;
-	float circle_radius = 1;
+	float circle_smoothness = circle_radius * 0.125;
+	
 	float circle = 1. - smoothstep( circle_radius - circle_smoothness, circle_radius, dist);
 
-	return vec4(vec3(1), circle);
+	return circle;
 }
 
 #endif
