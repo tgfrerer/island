f2543e6bc10b48d763cf36c1e8dc7d3b09fba795
tgfrerer <tim@poniesandlight.co.uk>
2018-11-28 19:18:54 +0000

    update app project generator
    + fix an issue where app name would be given recursively if template was
    named similar as app

--- scripts/create_app.py
+++ scripts/create_app.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from argparse import ArgumentParser
 from os import path
 from os import makedirs
 from os import rename
 from os import system
+from os import listdir
+import os
 import shutil
+from shutil import copystat
+import stat
+import errno
+
+class Error(EnvironmentError):
+    pass
 
 #Arguments 
 
@@ -27,27 +35,75 @@ def to_titled_camel_case(snake_str):
 	# We capitalize the first letter of each component.
 	return ''.join(x.title() for x in components)
 
+def copy_tree(src, dst, template_name, app_name, symlinks=False, ignore=None, copy_function=shutil.copy2,
+			 ignore_dangling_symlinks=False):
+	
+	names = os.listdir(src)
 
-def copyTree(src, dest):
-### Copies a directory tree, ignores CMakeLists.txt.user files.
+	if ignore is not None:
+		ignored_names = ignore(src, names)
+	else:
+		ignored_names = set()
+
+	os.makedirs(dst)
+
+	errors = []
+	for name in names:
+		if name in ignored_names:
+			continue
+		print("name: %s" % name)
+		srcname = os.path.join(src, name)
+		dstname = os.path.join(dst, name.replace(template_name, app_name, 1))
+		print ("dstname: %s" % dstname)
+		try:
+			if os.path.islink(srcname):
+				linkto = os.readlink(srcname)
+				if symlinks:
+					# We can't just leave it to `copy_function` because legacy
+					# code with a custom `copy_function` may rely on copytree
+					# doing the right thing.
+					os.symlink(linkto, dstname)
+					shutil.copystat(srcname, dstname, follow_symlinks=not symlinks)
+				else:
+					# ignore dangling symlink if the flag is on
+					if not os.path.exists(linkto) and ignore_dangling_symlinks:
+						continue
+					# otherwise let the copy occur. copy2 will raise an error
+					copy_function(srcname, dstname)
+			elif os.path.isdir(srcname):
+				copy_tree(srcname, dstname, template_name, app_name, symlinks, ignore, copy_function)
+			else:
+				# Will raise a SpecialFileError for unsupported file types
+				# copy file
+				copy_function(srcname, dstname)
+		# catch the Error from the recursive copytree so that we can
+		# continue with other files
+		except Error as err:
+			errors.extend(err.args[0])
+		except EnvironmentError as why:
+			errors.append((srcname, dstname, str(why)))
 	try:
-		shutil.copytree(src, dest, ignore=shutil.ignore_patterns('CMakeLists.txt.user'))
-	except OSError as e:
-		# If the error was caused because the source wasn't a directory
-		if e.errno == errno.ENOTDIR:
-			shutil.copy(src, dest)
+		copystat(src, dst)
+	except OSError as why:
+		if WindowsError is not None and isinstance(why, WindowsError):
+			# Copying file access times may fail on Windows
+			pass
 		else:
-			print('Directory not copied. Error: %s' % e)
+			errors.append((src, dst, str(why)))
+	if errors:
+		raise Error(errors)
+	return dst
 
-app_name = args.app_name
-app_module_name = app_name + "_app"
-template_name = args.template_name
-app_name_camelcase_capitalised = to_titled_camel_case(app_name)
+
+app_name                            = args.app_name
+app_module_name                     = app_name + "_app"
+template_name                       = args.template_name
+app_name_camelcase_capitalised      = to_titled_camel_case(app_name)
 template_name_camelcase_capitalised = to_titled_camel_case(template_name)
-app_dir = ("./%s" % app_name)
-app_module_dir = ("%s/%s" % (app_dir, app_module_name))
-script_dir = path.split(path.realpath(__file__))[0] # directory where this script lives
-template_source_dir = ("%s/templates/app/%s" % (script_dir, template_name))
+app_dir                             = ("./%s" % app_name)
+app_module_dir                      = ("%s/%s" % (app_dir, app_module_name))
+script_dir                          = path.split(path.realpath(__file__))[0] # directory where this script lives
+template_source_dir                 = ("%s/templates/app/%s" % (script_dir, template_name))
 
 
 print("App name: %s" % app_name)
@@ -59,21 +115,6 @@ print("Template source directory: %s" % template_source_dir)
 def process_directory_recursively(dir_path, replacements):
 	for item in replacements:
 		system("find -P %s -type f -exec sed -i 's/%s/g' {} +" % (dir_path, item))
-
-# Go through all files in target directory recursively,
-# and apply replacements to file and directory names.
-def rename_target_files_recursively(dir_path, file_name_replacements):
-	# first recursively rename directories
-	for pair in file_name_replacements:
-		exec_str = "find -P %s -name '*%s*' -type d -exec bash -c 'mv \"$0\" \"${0/%s/%s}\"' {} +" % (dir_path, pair[0], pair[0], pair[1] )
-		# print (exec_str)
-		system(exec_str)
-	# then recursively rename files in directories
-	for pair in file_name_replacements:
-		exec_str = "find -P %s -name '*%s*' -type f -exec bash -c 'mv \"$0\" \"${0/%s/%s}\"' {} -- \;" % (dir_path, pair[0], pair[0], pair[1] )
-		# print (exec_str)
-		system(exec_str)
-
 
 # check if directory with given module name exists
 # if yes, exit with an error message
@@ -93,13 +134,10 @@ else:
 
 	# ---------- invariant template source dir exists.
 
-	# Create a copy of the template in our target directory
-	copyTree(template_source_dir, app_dir)
+	# Create a copy of the template in our target directory.
+	# While copying template_name is substituted for app_name
+	copy_tree(template_source_dir, app_dir, template_name, app_name, ignore=shutil.ignore_patterns('CMakeLists.txt.user', 'build'))
 	
-	# Replace template filenames with our app filenames
-	file_name_replacements = [ [template_name, app_name] ]
-	rename_target_files_recursively(app_dir, file_name_replacements)
-
 	# Replace template names with app names in all target files
 	replacements = {'%s/%s' % (template_name, app_name), 
 					'%s/%s' % (template_name_camelcase_capitalised, app_name_camelcase_capitalised)}
