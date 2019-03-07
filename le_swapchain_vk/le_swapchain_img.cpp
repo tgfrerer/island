22decb28ce55431236e4a9a40592198a4b1325f2
tgfrerer <tim@poniesandlight.co.uk>
2019-03-07 19:41:16 +0000

    update image swapchain
    + add animated gif option

--- le_swapchain_vk/le_swapchain_img.cpp
+++ le_swapchain_vk/le_swapchain_img.cpp
@@ -283,7 +283,8 @@ static le_swapchain_o *swapchain_img_create( const le_swapchain_vk_api::swapchai
 
 		const char *commandLines[] = {
 		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -vcodec h264_nvenc -preset llhq -rc:v vbr_minqp -qmin:v 19 -qmax:v 21 -b:v 2500k -maxrate:v 5000k -profile:v high isl%s.mp4",
-		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -vcodec h264_nvenc -preset llhq -rc:v vbr_minqp -qmin:v 19 -qmax:v 21 -b:v 2500k -maxrate:v 5000k -r 30 -profile:v high isl%s.mp4",
+		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -filter_complex \"[0:v] fps=30,split [a][b];[a] palettegen [p];[b][p] paletteuse\" isl%s.gif",
+		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -vcodec h264_nvenc -preset llhq -rc:v vbr_minqp -qmin:v 0 -qmax:v 10 -b:v 2500k -maxrate:v 5000k -r 30 -profile:v high isl%s.mp4",
 		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -preset fast -y -pix_fmt yuv420p isl%s.mp4",
 		    "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 isl%s_%%03d.png",
 		};
