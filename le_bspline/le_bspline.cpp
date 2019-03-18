f0e0cfe56c3b899702b8027045994402f5b7282f
tgfrerer <tim@poniesandlight.co.uk>
2019-03-18 18:53:02 +0000

    implement b splines (first draft)

---
+++ le_bspline/le_bspline.cpp
@@ -0,0 +1,193 @@
+#include "le_bspline.h"
+#include "pal_api_loader/ApiRegistry.hpp"
+
+/* B-Spline methods
+ *
+ * Implementation based on <https://github.com/thibauts/b-spline/>
+ */
+
+#include <vector>
+#include "glm/glm.hpp"
+#include <iostream>
+#include <iomanip>
+
+using Vertex = glm::vec2;
+
+struct le_bspline_o {
+	uint32_t degree = 1; // must be at least 1
+
+	uint16_t dirty  = 1;
+	uint16_t closed = 0;
+
+	std::vector<float>  knots;
+	std::vector<Vertex> points;
+	std::vector<float>  weight;
+
+	std::vector<Vertex> polyline;
+};
+
+// ----------------------------------------------------------------------
+
+static le_bspline_o *le_bspline_create() {
+	auto self = new le_bspline_o();
+	return self;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_bspline_destroy( le_bspline_o *self ) {
+	delete self;
+}
+// ----------------------------------------------------------------------
+
+static void le_bspline_set_degree( le_bspline_o *self, uint32_t degree ) {
+	self->degree = degree;
+}
+
+// ----------------------------------------------------------------------
+static void le_bspline_set_closed( le_bspline_o *self, bool closed ) {
+	self->closed = closed;
+}
+// ----------------------------------------------------------------------
+static void le_bspline_set_points( le_bspline_o *self, Vertex const *points, size_t const num_points ) {
+	self->points = {points, points + num_points};
+}
+
+// ----------------------------------------------------------------------
+static void le_bspline_set_knots( le_bspline_o *self, float const *knots, size_t const num_knots ) {
+	self->knots = {knots, knots + num_knots};
+}
+
+// ----------------------------------------------------------------------
+static void le_bspline_set_weights( le_bspline_o *self, float const *weights, size_t const num_weights ) {
+	self->weight = {weights, weights + num_weights};
+}
+
+// ----------------------------------------------------------------------
+static bool le_bspline_trace( le_bspline_o *self, size_t resolution ) {
+
+	size_t n = self->points.size();
+
+	if ( self->degree < 1 ) {
+		assert( false ); // degree must be >= 1
+		return false;
+	}
+
+	if ( self->degree > ( n - 1 ) ) {
+		assert( false ); // degree must be less or equal to point count -1
+		return false;
+	}
+
+	// Initialise weights to 1 if no weights given.
+	// must have one weight per point
+	if ( self->weight.empty() ) {
+		self->weight = std::vector<float>( n, 1.f );
+	} else {
+		// Ensure that a weight is given for each point
+		if ( self->weight.size() != self->points.size() ) {
+			assert( false );
+			return false;
+		}
+	}
+
+	// Initisalise knots if not given, check number
+	// of knots if knots given.
+	{
+		// If closed, add a second helping of (degree+1) to number of knots
+		size_t numKnots = n + ( self->degree + 1 ) * ( self->closed ? 2 : 1 );
+
+		if ( self->knots.empty() ) {
+			self->knots.reserve( numKnots );
+			for ( size_t i = 0; i != numKnots; i++ ) {
+				self->knots.push_back( i );
+			}
+		} else {
+			// must ensure that number of knots is correct
+			if ( self->knots.size() != numKnots ) {
+				assert( false );
+				return false;
+			}
+		}
+	}
+	// ---------| invariant: number of knots is number of points + degree + 1
+
+	size_t domain[ 2 ] = {self->degree, self->knots.size() - ( self->degree + 1 )};
+	float  low         = self->knots[ domain[ 0 ] ];
+	float  high        = self->knots[ domain[ 1 ] ];
+
+	self->polyline.clear();
+
+	size_t s = domain[ 0 ]; // current segment
+	for ( size_t r = 0; r < resolution; r++ ) {
+
+		// homogenous coordinates for points
+		std::vector<glm::vec3> v;
+
+		// Create homogenous coordinates for each point
+		{
+
+			const size_t numPoints     = self->points.size();
+			size_t       numIterations = numPoints;
+			if ( self->closed ) {
+				numIterations += self->degree + 1;
+			}
+
+			v.reserve( numIterations );
+			for ( size_t i = 0; i != numIterations; ++i ) {
+				auto &p = self->points[ i % numPoints ];
+				auto &w = self->weight[ i % numPoints ];
+				v.emplace_back( p.x * w, p.y * w, w );
+			}
+		}
+
+		float t = float( r ) / float( resolution );
+		t       = t * ( high - low ) + low; // map t to domain
+
+		for ( ; s < domain[ 1 ]; s++ ) {
+			if ( t >= self->knots[ s ] && t <= self->knots[ s + 1 ] ) {
+				break;
+			}
+		}
+
+		// l (level) goes from 1 to the curve degree + 1
+		for ( size_t l = 1; l <= self->degree + 1; ++l ) {
+			// build level l of the pyramid
+			for ( size_t i = s; i > ( s - self->degree - 1 + l ); i-- ) {
+				// std::cout << i << std::flush;
+				float alpha = ( t - self->knots[ i ] ) /
+				              ( self->knots[ i + self->degree + 1 - l ] - self->knots[ i ] );
+				v[ i ] = ( 1.f - alpha ) * v[ i - 1 ] + alpha * v[ i ];
+			}
+		}
+
+		// Convert back (unproject homogenous coordinates) and store in output vector.
+
+		self->polyline.emplace_back( v[ s ].x / v[ s ].z,
+		                             v[ s ].y / v[ s ].z );
+	}
+
+	return true;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_bspline_get_vertices_for_polyline( le_bspline_o *self, Vertex const **vertices, size_t *num_vertices ) {
+	*vertices     = self->polyline.data();
+	*num_vertices = self->polyline.size();
+};
+
+// ----------------------------------------------------------------------
+
+ISL_API_ATTR void register_le_bspline_api( void *api ) {
+	auto &le_bspline_i = static_cast<le_bspline_api *>( api )->le_bspline_i;
+
+	le_bspline_i.create                    = le_bspline_create;
+	le_bspline_i.destroy                   = le_bspline_destroy;
+	le_bspline_i.set_degree                = le_bspline_set_degree;
+	le_bspline_i.set_closed                = le_bspline_set_closed;
+	le_bspline_i.set_points                = le_bspline_set_points;
+	le_bspline_i.set_knots                 = le_bspline_set_knots;
+	le_bspline_i.set_weights               = le_bspline_set_weights;
+	le_bspline_i.trace                     = le_bspline_trace;
+	le_bspline_i.get_vertices_for_polyline = le_bspline_get_vertices_for_polyline;
+}
---
+++ le_bspline/le_bspline.h
@@ -0,0 +1,129 @@
+#ifndef GUARD_le_bspline_H
+#define GUARD_le_bspline_H
+
+/*
+ * A module to handle B splines (basis function splines)).
+ *
+ */
+
+#include <stdint.h>
+#include "pal_api_loader/ApiRegistry.hpp"
+
+#ifdef __cplusplus
+
+#	define ISL_ALLOW_GLM_TYPES
+// Life is terrible without 3d type primitives, so let's include some glm forward declarations
+#	ifdef ISL_ALLOW_GLM_TYPES
+#		include <glm/fwd.hpp>
+#	endif
+
+extern "C" {
+#endif
+
+struct le_bspline_o;
+
+void register_le_bspline_api( void *api );
+
+// clang-format off
+struct le_bspline_api {
+
+#ifdef ISL_ALLOW_GLM_TYPES
+	typedef glm::vec2 Vertex;
+#else
+	struct Vertex{
+		float x;
+		float y;
+	};
+#endif
+
+
+	static constexpr auto id      = "le_bspline";
+	static constexpr auto pRegFun = register_le_bspline_api;
+
+	struct le_bspline_interface_t {
+
+		le_bspline_o *       ( * create                   ) ( );
+		void                 ( * destroy                  ) ( le_bspline_o* self );
+
+		void                 (* set_degree                ) ( le_bspline_o* self, uint32_t degree );
+		void                 (* set_closed                ) ( le_bspline_o* self, bool closed);
+		void                 (* set_points                ) ( le_bspline_o* self, Vertex const * points, size_t const num_points );
+		void                 (* set_knots                 ) ( le_bspline_o* self, float const * knots, size_t const num_knots );
+		void                 (* set_weights               ) ( le_bspline_o* self, float const* weights, size_t const num_weights );
+		bool                 (* trace                     ) ( le_bspline_o* self, size_t resolution );
+		void                 (* get_vertices_for_polyline ) ( le_bspline_o* self, Vertex const ** vertices, size_t * num_vertices );
+	};
+
+	le_bspline_interface_t       le_bspline_i;
+};
+// clang-format on
+
+#ifdef __cplusplus
+} // extern c
+
+namespace le_bspline {
+#	ifdef PLUGINS_DYNAMIC
+const auto api = Registry::addApiDynamic<le_bspline_api>( true );
+#	else
+const auto api = Registry::addApiStatic<le_bspline_api>();
+#	endif
+
+static const auto &le_bspline_i = api -> le_bspline_i;
+
+} // namespace le_bspline
+
+class LeBspline : NoCopy, NoMove {
+
+	le_bspline_o *self;
+
+  public:
+	LeBspline()
+	    : self( le_bspline::le_bspline_i.create() ) {
+	}
+
+	LeBspline &setDegree( uint32_t const &degree ) {
+		le_bspline::le_bspline_i.set_degree( self, degree );
+		return *this;
+	}
+
+	LeBspline &setClosed( bool closed ) {
+		le_bspline::le_bspline_i.set_closed( self, closed );
+		return *this;
+	}
+
+	LeBspline &setPoints( le_bspline_api::Vertex const *points, size_t numPoints ) {
+		le_bspline::le_bspline_i.set_points( self, points, numPoints );
+		return *this;
+	}
+
+	LeBspline &setKnots( float const *knots, size_t numKnots ) {
+		le_bspline::le_bspline_i.set_knots( self, knots, numKnots );
+		return *this;
+	}
+
+	LeBspline &setWeights( float const *weights, size_t numWeights ) {
+		le_bspline::le_bspline_i.set_weights( self, weights, numWeights );
+		return *this;
+	}
+
+	bool trace( size_t resolution ) {
+		return le_bspline::le_bspline_i.trace( self, resolution );
+	}
+
+	LeBspline &getVerticesForPolyline( le_bspline_api::Vertex const **pVertices, size_t *numVertices ) {
+		le_bspline::le_bspline_i.get_vertices_for_polyline( self, pVertices, numVertices );
+		return *this;
+	}
+
+	~LeBspline() {
+		le_bspline::le_bspline_i.destroy( self );
+	}
+
+	operator auto() {
+		return self;
+	}
+};
+
+#endif // __cplusplus
+
+#endif
