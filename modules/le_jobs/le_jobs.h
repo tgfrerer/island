27347302bf317b8188df6554f58805661db2f103
tgfrerer <tim@poniesandlight.co.uk>
2019-11-05 14:50:17 +0000

    add documentation

--- modules/le_jobs/le_jobs.h
+++ modules/le_jobs/le_jobs.h
@@ -28,18 +28,36 @@ struct le_jobs_api {
 		counter_t *complete_counter = nullptr; // owned by le_job_manager, counter to decrement when job completes
 	};
 
-	/* Initialise job system. This needs to be called only once
+	/* Initialise job system: This needs to be called only once,
 	 * before any other method involving the job system; 
+	 * 
 	 * `num_threads` tells us how many worker threads to initialise.
 	 */
 	void ( * initialize                ) ( size_t num_threads );
 	void ( * terminate                 ) ( );
 
-
+	/* Adds num_jobs to the job system queue, and immediately starts running them.
+	 * 
+	 * Allocates a counter within the job system, and initialises the counter's value
+	 * with `num_jobs`. Each jobs decrements counter once it completes.
+	 * 
+	 * Once all jobs are complete `counter` will be at 0.
+     *
+	 */
 	void ( * run_jobs                  ) ( le_job_o* jobs, uint32_t num_jobs, counter_t** counter );
+
+	/* Wait until counter == target value.
+	 * 
+	 * When called on the main thread, this method will spin-lock until counter is at target value.
+	 * When called from within the job system, this method will yield until counter is at target value.
+	 * 
+	 * Once counter has reached target value, the counter is freed within the job system,
+	 * and the method returns.
+	 * 
+	 */
 	void ( * wait_for_counter_and_free ) ( counter_t* counter, uint32_t target_value );
 
-	void    (* yield                )(void);
+	void (* yield                      ) ( void );
 
 	// return id of current worker thread (0..MAX_THREADS), or -1 if called from outside job system.
 	int32_t (* get_current_worker_id)(void); 
