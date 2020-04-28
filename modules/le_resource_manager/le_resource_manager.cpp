5ecefa57be34168eac01116addd006dde41f0883
tgfrerer <tim@poniesandlight.co.uk>
2020-04-28 17:10:36 +0100

    [resource_manager] add resource_manager
    implement resource_manager, a helper module which makes it easier to
    upload images, and keep track of uploaded images.

---
+++ modules/le_resource_manager/le_resource_manager.cpp
@@ -0,0 +1,206 @@
+#include "le_resource_manager.h"
+#include "le_core/le_core.h"
+#include "le_renderer/le_renderer.h"
+#include "le_pixels/le_pixels.h"
+
+#include <string>
+#include <vector>
+#include <assert.h>
+// ----------------------------------------------------------------------
+
+struct le_resource_manager_o {
+
+	struct image_data_layer_t {
+		le_pixels_o *pixels;
+		std::string  path;
+		bool         was_uploaded = false;
+	};
+
+	struct resource_item_t {
+		le_resource_handle_t            image_handle;
+		le_resource_info_t              image_info;
+		std::vector<image_data_layer_t> image_layers; // must have at least one element
+	};
+
+	std::vector<resource_item_t> resources;
+};
+
+// TODO: we need a way to set the correct data for resource_items inside the reource manager.
+// + instantiate resource manager with app
+// + upload images via resource manager with app
+// + add destructor for resource manager
+// we need to add a way to remove resources from the manager, so that we don't just leak resources
+// once we have uploaded them.
+
+// ----------------------------------------------------------------------
+static bool setupTransferPass( le_renderpass_o *pRp, void *user_data ) {
+	le::RenderPass rp{ pRp };
+	auto           manager = static_cast<le_resource_manager_o const *>( user_data );
+
+	if ( manager->resources.empty() ) {
+		return false;
+	}
+
+	// --------| invariant: some elements need upload
+
+	bool needsTransfer = false;
+
+	for ( auto const &r : manager->resources ) {
+
+		bool uses_resource = false;
+
+		for ( auto const &layer : r.image_layers ) {
+			if ( layer.was_uploaded == false ) {
+				uses_resource = true;
+				break;
+			}
+		}
+
+		if ( uses_resource ) {
+			rp.useImageResource( r.image_handle, { LE_IMAGE_USAGE_TRANSFER_DST_BIT } );
+			needsTransfer = true;
+		}
+	}
+
+	return needsTransfer;
+}
+
+// ----------------------------------------------------------------------
+static void execTransferPass( le_command_buffer_encoder_o *pEncoder, void *user_data ) {
+	le::Encoder encoder{ pEncoder };
+	auto        manager = static_cast<le_resource_manager_o *>( user_data );
+
+	// we will probably end up with a number of write operations which will all target the same image resource,
+	// but will write into different aspects of memory associated with the resource.
+
+	// First figure out whether we must write to an image at all
+
+	using namespace le_pixels;
+
+	for ( auto &r : manager->resources ) {
+
+		uint32_t const num_layers     = uint32_t( r.image_layers.size() );
+		uint32_t const image_width    = r.image_info.image.extent.width;
+		uint32_t const image_height   = r.image_info.image.extent.height;
+		uint32_t const image_depth    = r.image_info.image.extent.depth;
+		uint32_t const num_mip_levels = r.image_info.image.mipLevels;
+
+		for ( uint32_t layer = 0; layer != num_layers; layer++ ) {
+
+			// we can fill in the correct handling for mutliple mip levels later.
+			// for now, assert that there is exatcly one mip level.
+			if ( r.image_layers[ layer ].was_uploaded ) {
+				continue;
+			}
+
+			// --------| invariant: layer was not yet uploaded.
+
+			for ( uint32_t mip_level = 0; mip_level != num_mip_levels; mip_level++ ) {
+
+				assert( mip_level == 0 && "mip level greater than 0 not implemented" );
+				uint32_t width  = image_width >> mip_level;
+				uint32_t height = image_height >> mip_level;
+				uint32_t depth  = image_depth;
+
+				le_write_to_image_settings_t write_info =
+				    le::WriteToImageSettingsBuilder()
+				        .setDstMiplevel( mip_level )
+				        .setNumMiplevels( 1 )
+				        .setArrayLayer( layer ) // faces are indexed: +x, -x, +y, -y, +z, -z
+				        .setImageH( height )
+				        .setImageW( width )
+				        .build();
+				auto     pixels    = r.image_layers[ layer ].pixels;
+				auto     info      = le_pixels_i.get_info( pixels );
+				uint32_t num_bytes = info.byte_count; // TODO: make sure to get correct byte count for mip level, or compressed image.
+				void *   bytes     = le_pixels_i.get_data( pixels );
+
+				encoder.writeToImage( r.image_handle, write_info, bytes, num_bytes );
+			}
+			r.image_layers[ layer ].was_uploaded = true;
+		}
+	}
+}
+
+// ----------------------------------------------------------------------
+
+static void le_resource_manager_update( le_resource_manager_o *manager, le_render_module_o *module ) {
+	using namespace le_renderer;
+
+	// TODO: load any images if you detect that their source on disk has changed.
+
+	for ( auto &r : manager->resources ) {
+		render_module_i.declare_resource( module, r.image_handle, r.image_info );
+	}
+
+	auto renderPassTransfer =
+	    le::RenderPass( "xfer_le_resource_manager", LE_RENDER_PASS_TYPE_TRANSFER )
+	        .setSetupCallback( manager, setupTransferPass )  // decide whether to go forward
+	        .setExecuteCallback( manager, execTransferPass ) //
+	    ;
+
+	render_module_i.add_renderpass( module, renderPassTransfer );
+}
+
+// ----------------------------------------------------------------------
+// NOTE: You must provide an array of paths in image_paths, and the
+// array's size must match `image_info.image.arrayLayers`
+// Most meta-data about the image file is loaded via image_info
+static void le_resource_manager_add_item( le_resource_manager_o *     self,
+                                          le_resource_handle_t const *image_handle,
+                                          le_resource_info_t const *  image_info,
+                                          char const **               image_paths ) {
+
+	le_resource_manager_o::resource_item_t item{};
+
+	item.image_handle = *image_handle;
+	item.image_info   = *image_info;
+	item.image_layers.reserve( image_info->image.arrayLayers );
+
+	for ( size_t i = 0; i != item.image_info.image.arrayLayers; ++i ) {
+		le_resource_manager_o::image_data_layer_t layer_data{};
+		layer_data.path         = std::string{ image_paths[ i ] };
+		layer_data.was_uploaded = false;
+
+		layer_data.pixels = nullptr; // TODO: should we load image here immediately?
+		// FIXME: pixels paramter need to be inferred via image parameters
+		layer_data.pixels = le_pixels::le_pixels_i.create( layer_data.path.c_str(), 4, le_pixels_info::Type::eUInt8 );
+
+		item.image_layers.emplace_back( layer_data );
+	}
+
+	self->resources.emplace_back( item );
+}
+
+// ----------------------------------------------------------------------
+
+static le_resource_manager_o *le_resource_manager_create() {
+	auto self = new le_resource_manager_o{};
+	return self;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_resource_manager_destroy( le_resource_manager_o *self ) {
+
+	using namespace le_pixels;
+
+	for ( auto &r : self->resources ) {
+		for ( auto &l : r.image_layers ) {
+			if ( l.pixels ) {
+				le_pixels_i.destroy( l.pixels );
+				l.pixels = nullptr;
+			}
+		}
+	}
+	delete ( self );
+}
+
+LE_MODULE_REGISTER_IMPL( le_resource_manager, api ) {
+	auto &le_resource_manager_i = static_cast<le_resource_manager_api *>( api )->le_resource_manager_i;
+
+	le_resource_manager_i.create   = le_resource_manager_create;
+	le_resource_manager_i.destroy  = le_resource_manager_destroy;
+	le_resource_manager_i.update   = le_resource_manager_update;
+	le_resource_manager_i.add_item = le_resource_manager_add_item;
+}
---
+++ modules/le_resource_manager/le_resource_manager.h
@@ -0,0 +1,135 @@
+#ifndef GUARD_le_resource_manager_H
+#define GUARD_le_resource_manager_H
+
+/*
+
+# ResourceManager
+
+Helper module for dealing with image resources. ResourceManager automatically
+loads image resources from file, and uploads image resources, and declares
+image resources to a rendermodule.
+
+Once an image was uploaded, it will not be transferred again, ResourceManager
+keeps track of uploaded images.
+
+## Usage
+
+    // In app definition:
+
+    struct app_o {
+        ...
+        LeResourceManager resource_manager;
+        ...
+    }
+
+    // In app.setup():
+
+        auto image_handle = LE_IMG_RESOURCE( "test_image" );
+        char const * path = "testimage.png";
+        auto image_info =
+            le::ImageInfoBuilder()
+                .setImageType( le::ImageType::e2D )
+                .setExtent( 1024, 1024 )
+                .build();
+
+        app->resource_manager.add_item(image_handle, image_info, &path);
+
+
+    // In app.update():
+
+    LeResourceManager.update(render_module);
+
+Call update with the rendermodule you want to use the resources with.
+
+* * * 
+    
+If you want to upload multiple layers for images - for cubemap images for example -
+you can specify multiple paths. NOTE you must specify the number of image array layers
+when you specify the image info for the resource
+    
+    // In app.setup():
+
+        auto cube_image = LE_IMG_RESOURCE( "cube_image" );
+        auto image_info =
+            le::ImageInfoBuilder()
+                .setImageType( le::ImageType::e2D )
+                .setExtent( 1024, 1024 )
+                .setCreateFlags( LE_IMAGE_CREATE_CUBE_COMPATIBLE_BIT )
+                .setArrayLayers( 6 )
+                .build();
+    
+        char const *paths[] = {
+            "./local_resources/cubemap/0.png",
+            "./local_resources/cubemap/1.png",
+            "./local_resources/cubemap/2.png",
+            "./local_resources/cubemap/3.png",
+            "./local_resources/cubemap/4.png",
+            "./local_resources/cubemap/5.png",
+        };
+        
+        app->resource_manager.add_item( cube_image, image_info, paths );
+
+*/
+
+#include "le_core/le_core.h"
+
+struct le_resource_manager_o;
+struct le_render_module_o;   // ffdecl. (from le_renderer)
+struct le_resource_handle_t; // ffdecl. (from le_renderer)
+struct le_resource_info_t;   // ffdecl. (from le_renderer)
+
+// clang-format off
+struct le_resource_manager_api {
+
+	struct le_resource_manager_interface_t {
+
+		le_resource_manager_o *  ( * create    ) ( );
+		void                     ( * destroy   ) ( le_resource_manager_o* self );
+		void                     ( * update    ) ( le_resource_manager_o* self, le_render_module_o* module );
+        void                     ( * add_item  ) ( le_resource_manager_o* self, le_resource_handle_t const * image_handle, le_resource_info_t const * image_info, char const ** arr_image_paths);
+
+	};
+
+	le_resource_manager_interface_t       le_resource_manager_i;
+};
+// clang-format on
+
+LE_MODULE( le_resource_manager );
+LE_MODULE_LOAD_DEFAULT( le_resource_manager );
+
+#ifdef __cplusplus
+
+namespace le_resource_manager {
+static const auto &api                   = le_resource_manager_api_i;
+static const auto &le_resource_manager_i = api -> le_resource_manager_i;
+} // namespace le_resource_manager
+
+class LeResourceManager : NoCopy, NoMove {
+
+	le_resource_manager_o *self;
+
+  public:
+	LeResourceManager()
+	    : self( le_resource_manager::le_resource_manager_i.create() ) {
+	}
+
+	~LeResourceManager() {
+		le_resource_manager::le_resource_manager_i.destroy( self );
+	}
+
+	void update( le_render_module_o *module ) {
+		le_resource_manager::le_resource_manager_i.update( self, module );
+	}
+
+	void add_item( le_resource_handle_t const &image_handle, le_resource_info_t const &image_info, char const **arr_image_paths ) {
+		le_resource_manager::le_resource_manager_i.add_item( self, &image_handle, &image_info, arr_image_paths );
+	}
+
+	operator auto() {
+		return self;
+	}
+};
+
+#endif // __cplusplus
+
+#endif
