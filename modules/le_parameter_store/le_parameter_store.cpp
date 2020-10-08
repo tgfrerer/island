6838433165ca5b16aed6e4b2de92aee3472e2d42
tgfrerer <tim@poniesandlight.co.uk>
2020-10-08 10:43:44 +0100

    [parameter] add parameter store module
    first draft of parameter store module.
    
    + support for float, u32, i32, bool parameters
    + parameters have persistent address
    + parameter is owned by its parameter_store, it will live for the
    duration of its parent parameter store.

---
+++ modules/le_parameter_store/le_parameter_store.cpp
@@ -0,0 +1,161 @@
+#include "le_parameter_store.h"
+#include "le_core/le_core.h"
+#include <unordered_map>
+#include <string>
+
+// Implement load from file: jsmn
+// Implement store to json -- we write the serialiser ourselves. we just need to make sure it generates valid json.
+
+using Type = le_parameter_store_api::Type;
+
+struct le_parameter_o {
+	le_parameter_store_api::Type type;
+	union Data {
+		float    as_float[ 3 ];
+		uint32_t as_u32[ 3 ];
+		int32_t  as_i32[ 3 ];
+		bool     as_bool[ 3 ];
+		char     as_bytes[ 4 ][ 3 ];
+	};
+	Data value; // value, range_min, range_max
+};
+
+static float *le_parameter_set_float( le_parameter_o *self, float val ) {
+	if ( nullptr == self ) {
+		return nullptr;
+	}
+	self->type                = Type::eFloat;
+	self->value.as_float[ 0 ] = val;
+	self->value.as_float[ 1 ] = 0.0;
+	self->value.as_float[ 2 ] = 1.f;
+	return &self->value.as_float[ 0 ];
+}
+static uint32_t *le_parameter_set_u32( le_parameter_o *self, uint32_t val ) {
+	if ( nullptr == self ) {
+		return nullptr;
+	}
+	self->type              = Type::eU32;
+	self->value.as_u32[ 0 ] = val;
+	self->value.as_u32[ 1 ] = 0;
+	self->value.as_u32[ 2 ] = UINT32_MAX;
+	return &self->value.as_u32[ 0 ];
+}
+static int32_t *le_parameter_set_i32( le_parameter_o *self, int32_t val ) {
+	if ( nullptr == self ) {
+		return nullptr;
+	}
+	self->type              = Type::eI32;
+	self->value.as_i32[ 0 ] = val;
+	self->value.as_i32[ 1 ] = INT32_MIN;
+	self->value.as_i32[ 2 ] = INT32_MAX;
+	return &self->value.as_i32[ 0 ];
+}
+static bool *le_parameter_set_bool( le_parameter_o *self, bool val ) {
+	if ( nullptr == self ) {
+		return nullptr;
+	}
+	self->type               = Type::eBool;
+	self->value.as_bool[ 0 ] = val;
+	self->value.as_bool[ 1 ] = false;
+	self->value.as_bool[ 2 ] = true;
+	return &self->value.as_bool[ 0 ];
+}
+// Note: getters will return nullptr if type does not match internal parameter type.
+static float *le_parameter_as_float( le_parameter_o *self ) {
+	if ( nullptr == self || self->type != Type::eFloat ) {
+		return nullptr;
+	}
+	return &self->value.as_float[ 0 ];
+}
+static uint32_t *le_parameter_as_u32( le_parameter_o *self ) {
+	if ( nullptr == self || self->type != Type::eU32 ) {
+		return nullptr;
+	}
+	return &self->value.as_u32[ 0 ];
+}
+static int32_t *le_parameter_as_i32( le_parameter_o *self ) {
+	if ( nullptr == self || self->type != Type::eI32 ) {
+		return nullptr;
+	}
+	return &self->value.as_i32[ 0 ];
+}
+static bool *le_parameter_as_bool( le_parameter_o *self ) {
+	if ( nullptr == self || self->type != Type::eBool ) {
+		return nullptr;
+	}
+	return &self->value.as_bool[ 0 ];
+}
+static void le_parameter_set_type( le_parameter_o *self, Type type ) {
+	if ( nullptr == self ) {
+		return;
+	}
+	self->type = type;
+}
+static Type le_parameter_get_type( le_parameter_o *self ) {
+	if ( nullptr == self ) {
+		return Type::eUnknown;
+	}
+	return self->type;
+}
+
+// ----------------------------------------------------------------------
+struct le_parameter_store_o {
+	std::unordered_map<std::string, le_parameter_o> store;
+};
+
+// ----------------------------------------------------------------------
+
+static le_parameter_o *le_parameter_store_get_parameter( le_parameter_store_o *self, char const *name ) {
+	auto it = self->store.find( name );
+	if ( it == self->store.end() ) {
+		return nullptr;
+	}
+	return &it->second;
+}
+
+static le_parameter_o *le_parameter_store_add_parameter( le_parameter_store_o *self, char const *name ) {
+	return &self->store[ name ];
+}
+
+// ----------------------------------------------------------------------
+
+static le_parameter_store_o *le_parameter_store_create() {
+	auto self = new le_parameter_store_o();
+	return self;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_parameter_store_destroy( le_parameter_store_o *self ) {
+	delete self;
+}
+
+// ----------------------------------------------------------------------
+
+LE_MODULE_REGISTER_IMPL( le_parameter_store, api ) {
+
+	// -- parameter
+	auto &le_parameter_i = static_cast<le_parameter_store_api *>( api )->le_parameter_i;
+
+	le_parameter_i.as_bool  = le_parameter_as_bool;
+	le_parameter_i.as_float = le_parameter_as_float;
+	le_parameter_i.as_u32   = le_parameter_as_u32;
+	le_parameter_i.as_i32   = le_parameter_as_i32;
+
+	le_parameter_i.set_bool  = le_parameter_set_bool;
+	le_parameter_i.set_float = le_parameter_set_float;
+	le_parameter_i.set_u32   = le_parameter_set_u32;
+	le_parameter_i.set_i32   = le_parameter_set_i32;
+
+	le_parameter_i.get_type = le_parameter_get_type;
+	le_parameter_i.set_type = le_parameter_set_type;
+
+	// -- parameter store
+	auto &le_parameter_store_i = static_cast<le_parameter_store_api *>( api )->le_parameter_store_i;
+
+	le_parameter_store_i.create  = le_parameter_store_create;
+	le_parameter_store_i.destroy = le_parameter_store_destroy;
+
+	le_parameter_store_i.add_parameter = le_parameter_store_add_parameter;
+	le_parameter_store_i.get_parameter = le_parameter_store_get_parameter;
+}
---
+++ modules/le_parameter_store/le_parameter_store.h
@@ -0,0 +1,156 @@
+#ifndef GUARD_le_parameter_store_H
+#define GUARD_le_parameter_store_H
+
+#include "le_core/le_core.h"
+
+struct le_parameter_store_o;
+struct le_parameter_o;
+
+/*
+
+Parameters are heap-allocated bits of data which have a fixed address for the duration of the
+lifetime of parameter_store.
+
+Because of this, pointers handed out to parameters can be kept aound for as long as the parameter
+store object is alive.
+
+Each parameter is stored as an array<paramterType, 3>.
+
+Element [0] is the actual parameter value
+Element [1] is the parameter min value
+Element [2] is the parameter max value
+
+*/
+
+// clang-format off
+struct le_parameter_store_api {
+
+	enum Type {
+		eUnknown,
+		eFloat,
+		eU32,
+		eI32,
+		eBool,
+	};
+
+    struct le_parameter_interface_t {
+    
+        Type (*get_type)(le_parameter_o* parameter);
+        void (*set_type)(le_parameter_o* parameter, Type type);
+        
+        float*    (*set_float)( le_parameter_o *self, float val    );
+        uint32_t* (*set_u32  )( le_parameter_o *self, uint32_t val );
+        int32_t*  (*set_i32  )( le_parameter_o *self, int32_t val  );
+        bool*     (*set_bool )( le_parameter_o *self, bool val     );
+
+        float*    (*as_float)( le_parameter_o *self); // may return nullptr if wrong type 
+        uint32_t* (*as_u32  )( le_parameter_o *self); // may return nullptr if wrong type 
+        int32_t*  (*as_i32  )( le_parameter_o *self); // may return nullptr if wrong type 
+        bool*     (*as_bool )( le_parameter_o *self); // may return nullptr if wrong type 
+        
+    };
+
+	struct le_parameter_store_interface_t {
+
+		le_parameter_store_o * ( * create  ) ( );
+		void                   ( * destroy ) ( le_parameter_store_o* self );
+
+        le_parameter_o* (*get_parameter)(le_parameter_store_o* self, char const* name); // may return nullptr, if parameter not found.
+        le_parameter_o* (*add_parameter)(le_parameter_store_o* self, char const* name);
+
+	};
+
+	le_parameter_interface_t       le_parameter_i;
+	le_parameter_store_interface_t le_parameter_store_i;
+};
+// clang-format on
+
+LE_MODULE( le_parameter_store );
+LE_MODULE_LOAD_DEFAULT( le_parameter_store );
+
+#ifdef __cplusplus
+
+namespace le_parameter_store {
+static const auto &api                  = le_parameter_store_api_i;
+static const auto &le_parameter_store_i = api -> le_parameter_store_i;
+static const auto &le_parameter_i       = api -> le_parameter_i;
+} // namespace le_parameter_store
+
+class Parameter {
+	le_parameter_o *self;
+
+  public:
+	Parameter( le_parameter_o *param_ )
+	    : self( param_ ){};
+	Parameter()  = delete;
+	~Parameter() = default;
+	// There we go: rule of 5
+	Parameter( Parameter const &other )
+	    : self( other.self ){};
+	Parameter( Parameter &&other )
+	    : self( other.self ) {
+	}
+	Parameter &operator=( Parameter const &other ) {
+		self = other.self;
+		return *this;
+	}
+	Parameter &operator=( Parameter const &&other ) {
+		self = other.self;
+		return *this;
+	}
+	float *setFloat( float const &&val ) {
+		return le_parameter_store::le_parameter_i.set_float( self, static_cast<float const &&>( val ) );
+	}
+	int32_t *setI32( int32_t const &&val ) {
+		return le_parameter_store::le_parameter_i.set_i32( self, static_cast<int32_t const &&>( val ) );
+	}
+	uint32_t *setU32( uint32_t const &&val ) {
+		return le_parameter_store::le_parameter_i.set_u32( self, static_cast<uint32_t const &&>( val ) );
+	}
+	bool *setBool( bool const &&val ) {
+		return le_parameter_store::le_parameter_i.set_bool( self, static_cast<bool const &&>( val ) );
+	}
+	// ----
+	float *asFloat() {
+		return le_parameter_store::le_parameter_i.as_float( self );
+	}
+	int32_t *asI32() {
+		return le_parameter_store::le_parameter_i.as_i32( self );
+	}
+	uint32_t *asU32() {
+		return le_parameter_store::le_parameter_i.as_u32( self );
+	}
+	bool *asBool() {
+		return le_parameter_store::le_parameter_i.as_bool( self );
+	}
+};
+
+class LeParameterStore : NoCopy, NoMove {
+
+	le_parameter_store_o *self;
+
+  public:
+	LeParameterStore()
+	    : self( le_parameter_store::le_parameter_store_i.create() ) {
+	}
+
+	~LeParameterStore() {
+		le_parameter_store::le_parameter_store_i.destroy( self );
+	}
+
+	le_parameter_o *getParameter( char const *name ) {
+		return le_parameter_store::le_parameter_store_i.get_parameter( self, name );
+	}
+
+	le_parameter_o *addParameter( char const *name ) {
+		return le_parameter_store::le_parameter_store_i.add_parameter( self, name );
+	}
+
+	operator auto() {
+		return self;
+	}
+};
+
+#endif // __cplusplus
+
+#endif
