268879d48c5e16aacd60a650c43e71de519264cb
Philip Whitfield <philip@undef.ch>
2021-01-21 20:42:08 +0100

    add a directory watcher to file watcher

--- modules/le_file_watcher/le_file_watcher_linux.cpp
+++ modules/le_file_watcher/le_file_watcher_linux.cpp
@@ -15,10 +15,9 @@
 #	include <sys/inotify.h>
 #	include <unistd.h>
 
-
 // ----------------------------------------------------------------------
 
-struct Watch {
+struct FileWatch {
 	int                inotify_watch_handle = -1;
 	int                padding              = 0;
 	le_file_watcher_o *watcher_o;
@@ -29,12 +28,22 @@ struct Watch {
 	bool ( *callback_fun )( const char *path, void *user_data );
 };
 
+struct DirectoryWatch {
+	int                inotify_watch_handle = -1;
+	int                padding              = 0;
+	le_file_watcher_o *watcher_o;
+	std::string        path;
+	void *             callback_user_data = nullptr;
+	bool ( *callback_fun )( le_file_watcher_api::Event event, const char *path, void *user_data );
+};
+
 // ----------------------------------------------------------------------
 
 struct le_file_watcher_o {
-	int              inotify_socket_handle = -1;
-	int              padding               = 0;
-	std::list<Watch> mWatches;
+	int                       inotify_socket_handle = -1;
+	int                       padding               = 0;
+	std::list<FileWatch>      mFileWatches;
+	std::list<DirectoryWatch> mDirectoryWatches;
 };
 
 // ----------------------------------------------------------------------
@@ -49,10 +58,10 @@ static le_file_watcher_o *instance_create() {
 
 static void instance_destroy( le_file_watcher_o *instance ) {
 
-	for ( auto &w : instance->mWatches ) {
+	for ( auto &w : instance->mFileWatches ) {
 		inotify_rm_watch( instance->inotify_socket_handle, w.inotify_watch_handle );
 	}
-	instance->mWatches.clear();
+	instance->mFileWatches.clear();
 
 	if ( instance->inotify_socket_handle > 0 ) {
 		std::cout << "Closing inotify instance file handle: " << std::hex << instance->inotify_socket_handle << std::endl;
@@ -64,8 +73,8 @@ static void instance_destroy( le_file_watcher_o *instance ) {
 
 // ----------------------------------------------------------------------
 
-static int add_watch( le_file_watcher_o *instance, le_file_watcher_watch_settings const *settings ) noexcept {
-	Watch tmp;
+static int add_watch_file( le_file_watcher_o *instance, le_file_watcher_watch_settings const *settings ) noexcept {
+	FileWatch tmp;
 
 	auto tmp_path = std::filesystem::canonical( settings->filePath );
 
@@ -78,22 +87,46 @@ static int add_watch( le_file_watcher_o *instance, le_file_watcher_watch_setting
 
 	tmp.inotify_watch_handle = inotify_add_watch( instance->inotify_socket_handle, tmp.basename.c_str(), IN_CLOSE_WRITE );
 
-	instance->mWatches.emplace_back( std::move( tmp ) );
-	return tmp.inotify_watch_handle;
+	instance->mFileWatches.emplace_back( std::move( tmp ) );
+	return instance->mFileWatches.back().inotify_watch_handle; // tmp should not be used after move
+}
+
+static int add_watch_directory( le_file_watcher_o *instance, le_file_watcher_api::directory_settings const *settings ) noexcept {
+	DirectoryWatch tmp;
+
+	auto tmp_path = std::filesystem::canonical( settings->path );
+
+	tmp.path               = tmp_path.string();
+	tmp.watcher_o          = instance;
+	tmp.callback_fun       = settings->callback_fun;
+	tmp.callback_user_data = settings->callback_user_data;
+
+	tmp.inotify_watch_handle = inotify_add_watch( instance->inotify_socket_handle, tmp.path.c_str(), IN_CLOSE_WRITE | IN_CREATE | IN_DELETE | IN_MOVE );
+
+	instance->mDirectoryWatches.emplace_back( std::move( tmp ) );
+	return instance->mFileWatches.back().inotify_watch_handle;
 }
 
 // ----------------------------------------------------------------------
 
 static bool remove_watch( le_file_watcher_o *instance, int watch_id ) {
-	auto found_watch = std::find_if( instance->mWatches.begin(), instance->mWatches.end(), [ = ]( const Watch &w ) -> bool { return w.inotify_watch_handle == watch_id; } );
-	if ( found_watch != instance->mWatches.end() ) {
-		std::cout << "Removing inotify watch handle: " << std::hex << found_watch->inotify_watch_handle << std::endl;
+	auto found_watch = std::find_if( instance->mFileWatches.begin(), instance->mFileWatches.end(), [ = ]( const FileWatch &w ) -> bool { return w.inotify_watch_handle == watch_id; } );
+	if ( found_watch != instance->mFileWatches.end() ) {
+		std::cout << "Removing inotify file watch handle: " << std::hex << found_watch->inotify_watch_handle << std::endl;
 		inotify_rm_watch( instance->inotify_socket_handle, found_watch->inotify_watch_handle );
-		instance->mWatches.erase( found_watch );
+		instance->mFileWatches.erase( found_watch );
 		return true;
 	} else {
-		std::cout << "WARNING: " << __FUNCTION__ << ": could not find and thus remove watch with id:" << watch_id << std::endl;
-		return false;
+		auto found_dir_watch = std::find_if( instance->mDirectoryWatches.begin(), instance->mDirectoryWatches.end(), [ = ]( const DirectoryWatch &w ) -> bool { return w.inotify_watch_handle == watch_id; } );
+		if ( found_dir_watch != instance->mDirectoryWatches.end() ) {
+			std::cout << "Removing inotify directory watch handle: " << std::hex << found_dir_watch->inotify_watch_handle << std::endl;
+			inotify_rm_watch( instance->inotify_socket_handle, found_dir_watch->inotify_watch_handle );
+			instance->mDirectoryWatches.erase( found_dir_watch );
+			return true;
+		} else {
+			std::cout << "WARNING: " << __FUNCTION__ << ": could not find and thus remove watch with id:" << watch_id << std::endl;
+			return false;
+		}
 	}
 };
 
@@ -121,28 +154,78 @@ static void poll_notifications( le_file_watcher_o *instance ) {
 					continue;
 				}
 
-				std::string path          = ev->name;
-				const char *prev_filename = nullptr; // store previous filename to declutter printout
+				std::filesystem::path path          = ev->name;
+				const char *          prev_filename = nullptr; // store previous filename to declutter printout
 
-				// Only trigger on close-write
-				if ( ev->mask & IN_CLOSE_WRITE ) {
+				const auto log_trigger = [ & ]( const std::string &path, const std::string &src ) {
+					// Only print notice if it is for another filename:
+					if ( prev_filename != path.c_str() ) {
+						std::cout << "Watch triggered for: " << path << " [" << src << "]" << std::endl
+						          << std::flush;
+						prev_filename = path.c_str();
+					}
+				};
+
+				if ( ev->mask & IN_CREATE ) {
+					for ( auto const &w : instance->mDirectoryWatches ) {
+
+						if ( w.inotify_watch_handle == ev->wd ) {
+							log_trigger( path, w.path );
+
+							// Trigger Callback.
+							auto api_event =  std::filesystem::is_directory( w.path / path ) ? le_file_watcher_api::Event::DIRECTORY_CREATED : le_file_watcher_api::Event::FILE_CREATED;
+							( *w.callback_fun )( api_event, w.path.c_str(), w.callback_user_data );
+						}
+					}
+				} else if ( ev->mask & IN_DELETE ) {
+					for ( auto const &w : instance->mDirectoryWatches ) {
+
+						if ( w.inotify_watch_handle == ev->wd ) {
+
+							log_trigger( path, w.path );
+
+							// Trigger Callback.
+							auto api_event = path.extension().empty() ? le_file_watcher_api::Event::DIRECTORY_DELETED : le_file_watcher_api::Event::FILE_DELETED;
+							( *w.callback_fun )( api_event, w.path.c_str(), w.callback_user_data );
+						}
+					}
+				} else if ( ev->mask & IN_MOVE ) {
+					for ( auto const &w : instance->mDirectoryWatches ) {
+
+						if ( w.inotify_watch_handle == ev->wd ) {
+
+							log_trigger( path, w.path );
+
+							// Trigger Callback.
+							auto api_event = std::filesystem::is_directory( w.path / path ) ? le_file_watcher_api::Event::DIRECTORY_MOVED : le_file_watcher_api::Event::FILE_MOVED;
+							( *w.callback_fun )( api_event, w.path.c_str(), w.callback_user_data );
+						}
+					}
+				} else if ( ev->mask & IN_CLOSE_WRITE ) {
 
 					// We trigger *all* callbacks which watch the current file path
 					// For this, we must iterate through all watches and filter the
 					// ones which watch the event's file.
-					for ( auto const &w : instance->mWatches ) {
+					for ( auto const &w : instance->mFileWatches ) {
 
 						if ( w.inotify_watch_handle == ev->wd && w.filename == path ) {
 
-							// Only print notice if it is for another filename:
-							if ( prev_filename != path.c_str() ) {
-								std::cout << "Watch triggered for: " << path << " [" << w.basename << "]" << std::endl
-								          << std::flush;
-								prev_filename = path.c_str();
-							}
+							log_trigger( path, w.basename );
 
 							// Trigger Callback.
 							( *w.callback_fun )( w.path.c_str(), w.callback_user_data );
+						}
+					}
+
+					// now do the same for the directory watchers
+					for ( auto const &w : instance->mDirectoryWatches ) {
+
+						if ( w.inotify_watch_handle == ev->wd ) {
+
+							log_trigger( path, w.path );
+
+							// Trigger Callback.
+							( *w.callback_fun )( le_file_watcher_api::Event::FILE_MODIFIED, w.path.c_str(), w.callback_user_data );
 						}
 					}
 				}
@@ -157,13 +240,14 @@ static void poll_notifications( le_file_watcher_o *instance ) {
 // ----------------------------------------------------------------------
 
 LE_MODULE_REGISTER_IMPL( le_file_watcher, p_api ) {
-	auto  api                = reinterpret_cast<le_file_watcher_api *>( p_api );
-	auto &api_i              = api->le_file_watcher_i;
-	api_i.create             = instance_create;
-	api_i.destroy            = instance_destroy;
-	api_i.add_watch          = add_watch;
-	api_i.remove_watch       = remove_watch;
-	api_i.poll_notifications = poll_notifications;
+	auto  api                 = reinterpret_cast<le_file_watcher_api *>( p_api );
+	auto &api_i               = api->le_file_watcher_i;
+	api_i.create              = instance_create;
+	api_i.destroy             = instance_destroy;
+	api_i.add_watch           = add_watch_file;
+	api_i.add_watch_directory = add_watch_directory;
+	api_i.remove_watch        = remove_watch;
+	api_i.poll_notifications  = poll_notifications;
 };
 
 #endif
