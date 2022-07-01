c3d71657ec69893d1b364cddcdd397723749de96
tgfrerer <tim@poniesandlight.co.uk>
2022-07-01 19:22:52 +0100

    [backend] regenerate vk_to_str helpers

--- modules/le_backend_vk/private/le_backend_vk/update_vk_to_str_helpers.sh
+++ modules/le_backend_vk/private/le_backend_vk/update_vk_to_str_helpers.sh
@@ -2,11 +2,12 @@
 
 ../../../../scripts/codegen/gen_vk_enum_to_str.py <<- "EOF" | clang-format > vk_to_str_helpers.inl
 # Vulkan enum name
+VkAccessFlagBits2
 VkBufferUsageFlagBits
 VkFormat
-VkImageUsageFlagBits
-VkResult
-VkAccessFlagBits2
-VkPipelineStageFlagBits2
 VkImageLayout
+VkImageUsageFlagBits
+VkPipelineStageFlagBits2
+VkQueueFlagBits
+VkResult
 EOF
