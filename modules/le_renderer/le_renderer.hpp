15324c4485d8ebb7847f1d01963a49ee7c9a0996
tgfrerer <tim@poniesandlight.co.uk>
2023-02-17 16:27:54 +0000

    [renderer] cleanup

--- modules/le_renderer/le_renderer.hpp
+++ modules/le_renderer/le_renderer.hpp
@@ -155,10 +155,10 @@ class RendererInfoBuilder {
 		return mSwapchainInfoBuilder;
 	}
 
-	// If you want to add a swapchain later, you must have declared one of its type
-	// to the renderer. This will not automatically create the swapchain on renderer setup,
-	// but it will create the renderer and the backend with the necessary extensions
-	// for this swapchain to work.
+	/// If you want to add a swapchain later, you must have declared one of its type
+	/// to the renderer. This will not automatically create the swapchain on renderer setup,
+	/// but it will create the renderer and the backend with the necessary extensions
+	/// for this swapchain to work.
 	SwapchainInfoBuilder& declareSwapchain() {
 		info.swapchain_settings[ info.num_swapchain_settings ].defer_create = true;
 		return mSwapchainInfoBuilder;
