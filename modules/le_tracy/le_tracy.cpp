0d2a136877b4e94f9b53be6bacaf884d3d5a2c13
tgfrerer <tim@poniesandlight.co.uk>
2023-05-19 15:45:52 +0100

    [le_tracy] add logging
    you can optionally pass log messages to tracy by calling
    `LE_TRACY_ENABLE_LOG(-1)` in your app's setup code.

---
+++ modules/le_tracy/le_tracy.cpp
@@ -0,0 +1,127 @@
+#include "le_tracy.h"
+#include "le_core.h"
+#include "le_hash_util.h"
+#include "le_log.h"
+#include <mutex>
+#include "cassert"
+#include <memory>
+
+struct le_tracy_o {
+	uint32_t log_level_mask = 0;
+};
+
+static void** PP_TRACY_SINGLETON = nullptr; // set via registration method
+
+static le_tracy_o* produce_tracy() {
+	static std::mutex mtx;
+	auto              lock = std::unique_lock( mtx );
+
+	le_tracy_o* self = static_cast<le_tracy_o*>( *PP_TRACY_SINGLETON );
+	if ( nullptr == self ) {
+		*PP_TRACY_SINGLETON = new le_tracy_o();
+		self                = static_cast<le_tracy_o*>( *PP_TRACY_SINGLETON );
+	}
+	assert( self != nullptr );
+	return self;
+}
+
+// ----------------------------------------------------------------------
+// This method may not log via le::log, because otherwise there is a
+// chance of a deadlock.
+static void logger_callback( char const* chars, uint32_t num_chars, void* user_data ) {
+
+	// issue commands for tracy to log messages
+	TracyMessage( chars, num_chars );
+}
+
+// ----------------------------------------------------------------------
+
+// If we initialize an object from this and store it static,
+// then the destructor will get called when this module is being unloaded
+// this allows us to remove ourselves from listeners before the listener
+// gets destroyed.
+class LeLogSubscriber : NoCopy {
+  public:
+	explicit LeLogSubscriber( uint32_t log_level_mask )
+	    : handle( le_log::api->add_subscriber( logger_callback, nullptr, log_level_mask ) ) {
+		static auto logger = le::Log( "le_tracy" );
+		logger.debug( "Adding new Log subscriber for le_tracy with mask 0x%x", log_level_mask );
+	}
+	~LeLogSubscriber() {
+		static auto logger = le::Log( "le_tracy" );
+		logger.debug( "Removing Log subscriber" );
+		// we must remove the subscriber because it may get called before we have a chance to change the callback address -
+		// even callback forwarding won't help, because the reloader will log- and this log event will happen while the
+		// current module is not yet loaded, which means there is no valid code to run for the subscriber.
+		le_log::api->remove_subscriber( handle );
+	};
+
+  private:
+	uint64_t handle;
+};
+
+// we use a method like this so that we can intercept the moment the subscriber gets unloaded
+// when this library gets unloaded - in which case we want to be sure to unregister this
+// particular subscriber.
+// it will get registered again once it gets requested for the first time.
+std::unique_ptr<LeLogSubscriber>& produce_log_subscriber() {
+	static std::unique_ptr<LeLogSubscriber> subscriber;
+	return subscriber;
+}
+
+// ----------------------------------------------------------------------
+
+static void le_tracy_update_subscriber( uint32_t log_level_mask ) {
+	// we must reset / or re-create a subscriber.
+	if ( log_level_mask == 0 ) {
+		// remove subscriber if we are not listening to any messages
+		produce_log_subscriber().reset();
+	} else {
+		// add a new subscriber - this will implicitly remove the previous one if there
+		// previously has been a subscriber.
+		auto& subscriber = produce_log_subscriber();
+		subscriber       = std::make_unique<LeLogSubscriber>( log_level_mask );
+	}
+}
+
+static void le_tracy_enable_log( uint32_t log_level_mask ) {
+
+#ifdef TRACY_ENABLE
+
+	// this might initialise a tracy object in case none did previously exist.
+	le_tracy_o* self = produce_tracy();
+
+	if ( self->log_level_mask != log_level_mask ) {
+		le_tracy_update_subscriber( log_level_mask );
+	}
+
+	self->log_level_mask = log_level_mask;
+#else
+	auto logger = le::Log( "le_tracy" );
+	logger.warn( "Tracy is not enabled - enable tracy by adding compile definition `TRACY_ENABLE` to your project cmake file." );
+#endif
+}
+
+// ----------------------------------------------------------------------
+
+LE_MODULE_REGISTER_IMPL( le_tracy, api ) {
+	auto &le_tracy_i = static_cast<le_tracy_api *>( api )->le_tracy_i;
+
+	le_tracy_i.enable_log = le_tracy_enable_log;
+
+#ifdef LE_LOAD_TRACING_LIBRARY
+	LE_LOAD_TRACING_LIBRARY;
+#endif
+
+#ifdef TRACY_ENABLE
+	PP_TRACY_SINGLETON = le_core_produce_dictionary_entry( hash_64_fnv1a_const( "le_tracy_singleton" ) );
+
+	le_tracy_o* tracy = static_cast<le_tracy_o*>( *PP_TRACY_SINGLETON );
+
+	if ( tracy ) {
+		// if a tracy instance already exists, this is a sign that this module has been
+		// reloaded - in which case we want to re-register the subscriber to the log,
+		le_tracy_update_subscriber( tracy->log_level_mask );
+	}
+#endif
+}
--- modules/le_tracy/le_tracy.h
+++ modules/le_tracy/le_tracy.h
@@ -1,11 +1,42 @@
 #ifndef GUARD_le_tracy_H
 #define GUARD_le_tracy_H
 
+#include "le_core.h"
+
+/* Low-Overhead Profiling via Tracy
+ *
+ * Profiling is only enabled (and Tracy only compiled) if you add the
+ * following compiler definition into your topmost CMakeLists.txt file:
+ *
+ * 	add_compile_definitions( TRACY_ENABLE )
+ *
+ * To enable passing log messages to tracy, add LE_TRACY_ENABLE_LOG(-1)
+ * to where you initialize your main app.
+ *
+ * Every module which uses tracy must load the tracy library, you
+ * must do this via an explicit library load, by adding
+ *
+ * 	#ifdef LE_LOAD_TRACING_LIBRARY
+ *		LE_LOAD_TRACING_LIBRARY
+ * 	#endif
+ *
+ * To where you initialize this module's api pointers in its cpp file.
+ *
+ */
+
 #if defined( PLUGINS_DYNAMIC ) and defined( TRACY_ENABLE )
 #	define LE_LOAD_TRACING_LIBRARY \
 		le_core_load_library_persistently( "libTracyClient.so" )
 #else
 #	define LE_LOAD_TRACING_LIBRARY
+#endif
+
+#if defined( TRACY_ENABLE )
+#	define LE_TRACY_ENABLE_LOG( l ) \
+		le_tracy::api->le_tracy_i.enable_log( l )
+#else
+#	define LE_TRACY_ENABLE_LOG( l ) \
+		( l )
 #endif
 
 // We break our rule here which says that header files are not allowed
@@ -22,5 +53,35 @@
 #include "3rdparty/tracy/public/tracy/Tracy.hpp"
 
 //----------------------------------------------------------------------
+
+// we allow ourselves a tracy context object so that we can store
+// any auxiliary information associated with tracing in one place.
+// this includes a subscriber to the logger, if we so wish
+
+struct le_tracy_o;
+
+// clang-format off
+struct le_tracy_api {
+	
+	struct le_tracy_interface_t {
+		void            ( * enable_log                ) ( uint32_t log_messages_mask);
+		
+	};
+	
+	le_tracy_interface_t       le_tracy_i;
+};
+// clang-format on
+
+LE_MODULE( le_tracy );
+LE_MODULE_LOAD_DEFAULT( le_tracy );
+
+#ifdef __cplusplus
+
+namespace le_tracy {
+static const auto& api        = le_tracy_api_i;
+static const auto& le_tracy_i = api->le_tracy_i;
+} // namespace le_tracy
+
+#endif // __cplusplus
 
 #endif
