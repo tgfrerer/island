2c6e41aff25572c23525529bfd9af3725ab4df7e
tgfrerer <tim@poniesandlight.co.uk>
2024-02-05 10:53:10 +0000

    [image_decoder] whitespace + formatting changes

--- modules/le_core/shared/interfaces/le_image_decoder_interface.h
+++ modules/le_core/shared/interfaces/le_image_decoder_interface.h
@@ -18,6 +18,7 @@ struct le_image_decoder_o;
 struct le_image_decoder_format_o; // struct wrapper around le::Format
 
 // ----------------------------------------------------------------------
+// clang-format off
 
 struct le_image_decoder_interface_t {
 
@@ -26,14 +27,20 @@ struct le_image_decoder_interface_t {
 	uint64_t ( *get_api_version )() = []() -> uint64_t {
 		static constexpr uint64_t API_VERSION = 0ull << 48 | 0ull << 32 | 1ull << 16 | 0ull << 0;
 		return API_VERSION;
-	}; // static method - we should provide this so that we can make sure that implementers use compatible apis
+	};
 
 	le_image_decoder_o* ( *create_image_decoder )( char const* file_name );
-	void ( *destroy_image_decoder )( le_image_decoder_o* image_decoder_o );
+
+	void ( *destroy_image_decoder      )( le_image_decoder_o* image_decoder_o );
 
 	// load image data from file, and read it into a pre-allocated byte array at p_pixels
-	bool ( *read_pixels )( le_image_decoder_o* image_decoder_o, uint8_t* p_pixels, size_t pixels_byte_count );
+	bool ( *read_pixels                )( le_image_decoder_o* image_decoder_o, uint8_t* p_pixels, size_t pixels_byte_count );
 
-	void ( *set_requested_format )( le_image_decoder_o* image_decoder_o, le_image_decoder_format_o const* format );
+	// get image data description (this does should not read full image, only just the image metadata)
 	void ( *get_image_data_description )( le_image_decoder_o* image_decoder_o, le_image_decoder_format_o* p_format, uint32_t* w, uint32_t* h );
+
+	// set the format into which to read pixels
+	void ( *set_requested_format       )( le_image_decoder_o* image_decoder_o, le_image_decoder_format_o const* format );
 };
+
+// clang-format on
