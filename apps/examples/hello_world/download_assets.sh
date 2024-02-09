8b66c784f34be37fd5a6d650780286a0306186b7
tgfrerer <tim@poniesandlight.co.uk>
2024-02-09 12:19:02 +0000

    [examples] update download_assets
    add pushd recommendations via shellcheck

--- apps/examples/hello_world/download_assets.sh
+++ apps/examples/hello_world/download_assets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd resources
+pushd resources || exit
 curl -L "https://www.dropbox.com/s/vtcg03grmvryse8/images.tar.gz?dl=1" -o images.tar.gz
 tar xfv images.tar.gz
-popd
+popd || exit
