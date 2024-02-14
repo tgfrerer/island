01d5d44253fa7f114c5e6feb74f7f5fd1f83981a
tgfrerer <tim@poniesandlight.co.uk>
2024-02-14 12:18:43 +0000

    [shader_compiler] reformat whitespace

--- modules/le_shader_compiler/le_shader_compiler.h
+++ modules/le_shader_compiler/le_shader_compiler.h
@@ -21,21 +21,21 @@ struct le_shader_compiler_api {
 		le_shader_compiler_o*           (* create                ) ( );
 		void                            (* destroy               ) ( le_shader_compiler_o* self );
 
-		bool                            (* compile_source        ) ( le_shader_compiler_o *compiler, const char *sourceText, size_t sourceTextSize, const LeShaderSourceLanguageEnum& shader_source_language, const le::ShaderStageFlagBits& shaderType, const char *original_file_path, char const * macroDefinitionsStr, size_t macroDefinitionsStrSz, le_shader_compilation_result_o* result );
+		bool                    (* compile_source               ) ( le_shader_compiler_o *compiler, const char *sourceText, size_t sourceTextSize, const LeShaderSourceLanguageEnum& shader_source_language, const le::ShaderStageFlagBits& shaderType, const char *original_file_path, char const * macroDefinitionsStr, size_t macroDefinitionsStrSz, le_shader_compilation_result_o* result );
 
         // create a compilation result object - this is needed for compile_source 
-		le_shader_compilation_result_o* (* result_create         ) ( );
-		
+        le_shader_compilation_result_o* (* result_create ) ( );
+        void                    (* result_destroy        ) ( le_shader_compilation_result_o* res);
+
         /// \brief  iterate over include paths in current compilation result
 		/// \return false if no more paths, otherwise: true, and updates to pPath and pStrSz as side-effect
 		/// \note   lifetime of any pointers is tied to life-time of result object
-		bool                            (* result_get_included_files   ) ( le_shader_compilation_result_o* res, const char** pPath, void** it);
-		bool                            (* result_get_success    ) ( le_shader_compilation_result_o* res);
+		bool                    (* result_get_included_files ) ( le_shader_compilation_result_o* res, const char** pPath, void** it);
+		bool                    (* result_get_success        ) ( le_shader_compilation_result_o* res);
 
         // pAddr receives a pointer to spir-v binary code - this is guaranteed to be castable to uint32_t. 
-		void                            (* result_get_bytes      ) ( le_shader_compilation_result_o* res, const char** p_spir_v_bytes, size_t* pNumBytes);
+        void                    (* result_get_bytes          ) ( le_shader_compilation_result_o* res, const char** p_spir_v_bytes, size_t* pNumBytes);
 
-		void                            (* result_destroy        ) ( le_shader_compilation_result_o* res);
     };
 
 	compiler_interface_t       compiler_i;
