88e099877d674c0214a76a68522ce4d73d489bc1
tgfrerer <tim@poniesandlight.co.uk>
2024-05-17 15:20:33 +0100

    [spirv-reflect] fix windows includes
    fix header include paths for windows

--- modules/le_backend_vk/util/spirv_reflect/spirv_reflect.h
+++ modules/le_backend_vk/util/spirv_reflect/spirv_reflect.h
@@ -31,12 +31,16 @@ VERSION HISTORY
 #ifndef SPIRV_REFLECT_H
 #define SPIRV_REFLECT_H
 
-#if defined(SPIRV_REFLECT_USE_SYSTEM_SPIRV_H)
-#include <spirv/unified1/spirv.h>
-#else
-#include "./include/spirv/unified1/spirv.h"
-#endif
 
+#ifdef _MSC_VER
+    #include <spirv-headers/spirv.h>
+#else
+#   if defined(SPIRV_REFLECT_USE_SYSTEM_SPIRV_H)
+#include <spirv/unified1/spirv.h>
+#   else 
+#include "./include/spirv/unified1/spirv.h"
+#   endif
+#endif
 
 #include <stdint.h>
 #include <string.h>
