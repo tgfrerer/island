5e355869a13c308a256273b687a35d366a7a364d
tgfrerer <tim@poniesandlight.co.uk>
2025-06-10 10:27:13 +0100

    [pipeline] test for type name validity before hashing

--- modules/le_backend_vk/le_pipeline.cpp
+++ modules/le_backend_vk/le_pipeline.cpp
@@ -910,8 +910,9 @@ static void shader_module_update_reflection( le_shader_module_o* module ) {
 			}
 
 			// For buffer Types the name of the binding we're interested in is the type name.
-			if ( info.type == le::DescriptorType::eUniformBufferDynamic ||
-			     info.type == le::DescriptorType::eStorageBufferDynamic ) {
+			if ( (info.type == le::DescriptorType::eUniformBufferDynamic ||
+			     info.type == le::DescriptorType::eStorageBufferDynamic) &&
+				binding->type_description->type_name != nullptr) {
 				info.name_hash = hash_64_fnv1a( binding->type_description->type_name );
 			} else {
 				info.name_hash = hash_64_fnv1a( binding->name );
