ac7a18f93cb686dbfa2a5d97af5483fba3dcd845
tgfrerer <tim@poniesandlight.co.uk>
2025-07-11 17:56:53 +0100

    [le_font] update for new le_path api

--- modules/le_font/le_font.cpp
+++ modules/le_font/le_font.cpp
@@ -108,25 +108,25 @@ static void le_font_add_paths_for_glyph( le_font_o const* self, le_path_o* path,
 		case STBTT_vmove:
 			// a move signals the start of a new glyph
 			p0 = *offset + scale * glm::vec2{ pp->x + kern_advance, -pp->y };
-			le_path_i.move_to( path, &p0 );
+			le_path_operations_i.move_to( path, &p0 );
 			break;
 		case STBTT_vline:
 			// line from last position to this pos
 			p0 = *offset + scale * glm::vec2{ pp->x + kern_advance, -pp->y };
-			le_path_i.line_to( path, &p0 );
+			le_path_operations_i.line_to( path, &p0 );
 			break;
 		case STBTT_vcurve:
 			// quadratic bezier to pos
 			p0 = *offset + scale * glm::vec2{ pp->x + kern_advance, -pp->y };
 			p1 = *offset + scale * glm::vec2{ pp->cx + kern_advance, -pp->cy };
-			le_path_i.quad_bezier_to( path, &p0, &p1 );
+			le_path_operations_i.quad_bezier_to( path, &p0, &p1 );
 			break;
 		case STBTT_vcubic:
 			// cubic bezier to pos
 			p0 = *offset + scale * glm::vec2{ pp->x + kern_advance, -pp->y };
 			p1 = *offset + scale * glm::vec2{ pp->cx + kern_advance, -pp->cy };
 			p2 = *offset + scale * glm::vec2{ pp->cx1 + kern_advance, -pp->cy1 };
-			le_path_i.cubic_bezier_to( path, &p0, &p1, &p2 );
+			le_path_operations_i.cubic_bezier_to( path, &p0, &p1, &p2 );
 			break;
 		}
 	}
