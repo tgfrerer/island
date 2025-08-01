66ecc6021dc224c29e92a9dd6aaa0baaa3ef6416
tgfrerer <tim@poniesandlight.co.uk>
2025-08-01 11:09:21 +0100

    [bloom_pass] update glm includes

--- modules/le_bloom_pass/le_bloom_pass.cpp
+++ modules/le_bloom_pass/le_bloom_pass.cpp
@@ -4,9 +4,7 @@
 #include "le_renderer.hpp"
 #include "le_pipeline_builder.h"
 
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE // vulkan clip space is from 0 to 1
-#define GLM_FORCE_RIGHT_HANDED      // glTF uses right handed coordinate system, and we're following its lead.
-#include "3rdparty/src/glm/glm.hpp"
+#include "glm/glm.hpp"
 
 #include <algorithm> // for min/max
 #include <stdio.h>
