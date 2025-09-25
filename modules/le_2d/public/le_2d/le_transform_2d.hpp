235bb4e9918ecb0dede61a9765c699c958fa13ff
tgfrerer <tim@poniesandlight.co.uk>
2025-09-25 11:44:48 +0100

    [le_2d] cleanup le_transform_2d
    remove explicit calculations where we can express the math more clearly
    using glm. We keep hand-rolled math for inverse as it allows us to re-
    use the intermediate result for determinant, which would otherwise need
    to be calculated twice.

--- modules/le_2d/public/le_2d/le_transform_2d.hpp
+++ modules/le_2d/public/le_2d/le_transform_2d.hpp
@@ -1,60 +1,60 @@
 #pragma once
 #include "glm/glm.hpp"
 
-// Think of this as "first translate, then rotate(transform)"
+/// Structure representing Affine Transform
+///
+/// Think of this as "first translate, then rotate(transform)"
+///
 struct LeTransform2D {
-	glm::mat2 transform   = { 1, 0, 0, 1 }; // 2x2 matrix, column major
+
+	glm::mat2 transform = {
+	    { 1, 0 }, // x axis
+	    { 0, 1 }, // y axis
+	}; // 2x2 matrix, column major
+
 	glm::vec2 translation = { 0, 0 };
 
-	inline static LeTransform2D make_rotation_rad( float angle_rad ) {
+	// ----------
+
+	inline static LeTransform2D make_transform_rotation_rad( float angle_rad ) {
 		float       cosa  = cosf( angle_rad );
 		float       sina  = sinf( angle_rad );
-		LeTransform2D rot_m = { .transform = { cosa, sina, -sina, cosa }, .translation = { 0, 0 } };
-		return rot_m;
+		return {
+		    .transform = {
+		        { cosa, sina },
+		        { -sina, cosa },
+		    },
+		    .translation = { 0, 0 },
+		};
 	};
 
+	/// pre-multiplication operator
 	inline LeTransform2D operator*( LeTransform2D const& rhs ) const {
 		// Note: this has been checked against vello to
 		// make sure that we're using the same conventions.
 		auto const& t = this->transform;
 		auto const& o = rhs.transform;
 		return {
-		    {
-		        t * o
-		        // transform
-		        // t[ 0 ][ 0 ] * o[ 0 ][ 0 ] + t[ 1 ][ 0 ] * o[ 0 ][ 1 ],
-		        // t[ 0 ][ 1 ] * o[ 0 ][ 0 ] + t[ 1 ][ 1 ] * o[ 0 ][ 1 ],
-		        // t[ 0 ][ 0 ] * o[ 1 ][ 0 ] + t[ 1 ][ 0 ] * o[ 1 ][ 1 ],
-		        // t[ 0 ][ 1 ] * o[ 1 ][ 0 ] + t[ 1 ][ 2 ] * o[ 1 ][ 1 ],
-		    },
-		    {
-		        t * rhs.translation + this->translation
-		        // translation
-		        // t[ 0 ][ 0 ] * rhs.translation[ 0 ] + t[ 1 ][ 0 ] * rhs.translation[ 1 ] + this->translation[ 0 ],
-		        // t[ 0 ][ 1 ] * rhs.translation[ 0 ] + t[ 1 ][ 1 ] * rhs.translation[ 1 ] + this->translation[ 1 ],
-		    },
-		};
+		    t * o,
+		    t * rhs.translation + this->translation };
 	}
-
-	// inline float determinant() const {
-	// 	return transform[ 0 ][ 0 ] * transform[ 1 ][ 1 ] - transform[ 0 ][ 1 ] * transform[ 1 ][ 0 ];
-	// }
 
 	inline LeTransform2D inverse() const {
 
 		float inv_det = 1.f / glm::determinant( this->transform );
 
-		// float inv_det = 1.0 / determinant();
 		assert( inv_det == inv_det ); // test for NaN
 
 		auto result = LeTransform2D{
 		    .transform{
+		        // calculate inverse of transform
 		        inv_det * transform[ 1 ][ 1 ],
 		        -inv_det * transform[ 0 ][ 1 ],
 		        -inv_det * transform[ 1 ][ 0 ],
 		        inv_det * transform[ 0 ][ 0 ],
 		    },
 		    .translation{
+		        // reverse translation
 		        inv_det * ( transform[ 1 ][ 0 ] * translation[ 1 ] - transform[ 1 ][ 1 ] * translation[ 0 ] ),
 		        inv_det * ( transform[ 0 ][ 1 ] * translation[ 0 ] - transform[ 0 ][ 0 ] * translation[ 1 ] ),
 		    },
