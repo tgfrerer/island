49cc9d5374ad812a003065c6bf9a3c6300af04bd
tgfrerer <tim@poniesandlight.co.uk>
2025-09-29 11:51:03 +0100

    [rendergraph_visualizer] tweak api
    in the c++ api, make it optional to specify a separate rendergraph to
    visualize. if no separate rendergraph is given, then the rendergraph
    into which we are drawing into is being used to visualize.

--- modules/le_rendergraph_visualizer/le_rendergraph_visualizer.h
+++ modules/le_rendergraph_visualizer/le_rendergraph_visualizer.h
@@ -61,8 +61,9 @@ class LeRendergraphVisualizer : NoCopy, NoMove {
 		le_rendergraph_visualizer::le_rendergraph_visualizer_i.destroy( self );
 	}
 
-	void update( le_rendergraph_o* rendergraph_to_draw_into, le_image_resource_handle_t* target_image, le_rendergraph_o* rendergraph_to_visualize ) {
-		le_rendergraph_visualizer::le_rendergraph_visualizer_i.update( self, rendergraph_to_draw_into, target_image, rendergraph_to_visualize );
+	void update( le_rendergraph_o* rendergraph_to_draw_into, le_image_resource_handle_t* target_image, le_rendergraph_o* rendergraph_to_visualize = nullptr ) {
+		le_rendergraph_o* to_viz = rendergraph_to_visualize ? rendergraph_to_visualize : rendergraph_to_draw_into;
+		le_rendergraph_visualizer::le_rendergraph_visualizer_i.update( self, rendergraph_to_draw_into, target_image, to_viz );
 	}
 
 	void processEvents( LeUiEvent const* events, uint32_t num_events ) {
