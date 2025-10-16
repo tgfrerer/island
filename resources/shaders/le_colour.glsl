ba3b042eebbf14adc51c0875acfa6affc4844486
tgfrerer <tim@poniesandlight.co.uk>
2025-10-16 15:57:32 +0100

    [le_colour.glsl] update colour helper glsl

--- resources/shaders/le_colour.glsl
+++ resources/shaders/le_colour.glsl
@@ -67,11 +67,11 @@ vec4 rgba_linear_color_from_hex(in uint hexVal ){
 	return (result/255.f);
 }
 
-// // we're setting white point to what we expect our hdr 
-// // screen to have as default brightness, given in nits.
-// // we assume 350 or 400 for now.
+// we're setting white point to what we expect our hdr 
+// screen to have as default brightness, given in nits.
+// we assume 350 or 400 for now.
 
-// // This is adapted from: <https://panoskarabelas.com/blog/posts/hdr_in_under_10_minutes/>
+// This is adapted from: <https://panoskarabelas.com/blog/posts/hdr_in_under_10_minutes/>
 // vec3 linear_srgb_to_hdr10( in vec3 color, in const float white_point_in_nits)
 // {
 //     // Convert Rec.709 to Rec.2020 color space to broaden the palette
@@ -82,12 +82,12 @@ vec4 rgba_linear_color_from_hex(in uint hexVal ){
 //         { 0.0163916f, 0.0880132f, 0.8955950f }
 //     };   
 //     // color = color * from709to2020 ;
-//     // color = color * from709to2020 ;
-
+//      color = color * from709to2020 ;
+// 
 //     // Normalize HDR scene values ([0..>1] to [0..1]) for ST.2084 curve
 //     const float st2084_max = 10000.0f;
-//     color *= white_point_in_nits / st2084_max;
-
+//     // color *= white_point_in_nits / st2084_max;
+// 
 //     // Apply ST.2084 (PQ curve) for HDR10 standard
 //     //
 // 	// The original inverse-EOTF spec is Equation 5.2, pg.8 in: 
@@ -99,36 +99,36 @@ vec4 rgba_linear_color_from_hex(in uint hexVal ){
 //     const float c3 = 2392.0 / 4096.0 * 32;
 // 	vec3 cp             = pow(abs(color), vec3(m1));
 //     color               = pow((c1 + c2 * cp) / (1 + c3 * cp), vec3(m2));
-
+// 
 //     return color;
 // }
 
 
-// via http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+// Via http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
 // note that we pre-multiply because this is equivalent to a transpose
 // D65 Reference White 
-vec3 xyz_to_linear_srgb(in vec3 c){
+vec3 color_transform_xyz_to_linear_srgb(in vec3 c){
 	return c * mat3(
 	3.2404542, -1.5371385, -0.4985314,
 	-0.9692660,  1.8760108,  0.0415560,
  	0.0556434, -0.2040259,  1.0572252
-	);
+	) ;
 }
 
-// via http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+// Via http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
 // note that we pre-multiply because this is equivalent to a transpose
 // E Reference White = Equal energy Spectrum 
-vec3 xyz_to_cie_rgb(in vec3 c){
+vec3 color_transform_xyz_to_cie_rgb(in vec3 c){
 	return c * mat3(
  		2.3706743, -0.9000405, -0.4706338,
 		-0.5138850,  1.4253036,  0.0885814,
  		0.0052982, -0.0146949,  1.0093968
-	);
+	) ;
 }
 
-// this can also be found here: 
+// This can also be found here: 
 // https://antlerpost.com/colour-spaces/ST2084.html
-vec3 xyz_to_rec_2020(in vec3 c){
+vec3 color_transform_xyz_to_rec_2020(in vec3 c){
 	return c * mat3(
 		vec3( 1.71666343, -0.35567332, -0.25336809 ),
 		vec3( -0.66667384, 1.61645574, 0.0157683 ), 
@@ -136,9 +136,9 @@ vec3 xyz_to_rec_2020(in vec3 c){
 	);
 }
 
-// this can also be found here: 
+// This can also be found here: 
 // https://antlerpost.com/colour-spaces/ST2084.html
-vec3 xyz_to_p3(in vec3 c){
+vec3 color_transform_xyz_to_p3(in vec3 c){
 	return c * mat3(
         vec3(2.49349691, -0.93138362, -0.40271078),
         vec3(-0.82948897, 1.76266406, 0.02362469),
@@ -146,33 +146,60 @@ vec3 xyz_to_p3(in vec3 c){
 	);
 }
 
-// sRGB defines a color space - a gamut.
-// see also: <https://en.wikipedia.org/wiki/Rec._709>
-//
-// A color gamut can have a transfer function ("gamma")
-//
-// The opto-electronic transfer function (OETF) is the transfer function having the scene light as input and converting into the picture or video signal as output. 
-// The electro-optical transfer function (EOTF) is the transfer function having the picture or video signal as input and converting it into the linear light output of the display.
+// ----------------------------------------------------------------------  
+// Electro-Optical Transfer Functions (and their inverses)
+// See also: <https://registry.khronos.org/DataFormat/specs/1.3/dataformat.1.3.html#TRANSFER_CONVERSION>
+// ----------------------------------------------------------------------  
 
-// Converts a color from linear sRGB to non-linear sRGB gamma
-// this uses the correct OETF (opto-electrical transfer function)
-// based on the bt.709 standard: <https://en.wikipedia.org/wiki/Rec._709>
+// What is a transfer function and why should I care? 
 //
-vec3 linear_srgb_to_non_linear_srgb(vec3 linear_sRGB)
-{
-    bvec3 cutoff = lessThan(linear_sRGB, vec3(0.0031308));
-    vec3 higher = vec3(1.055)*pow(linear_sRGB, vec3(1.0/2.4)) - vec3(0.055);
-    vec3 lower = linear_sRGB * vec3(12.92);
+// sRGB defines a color space - a gamut. see also:
+// <https://en.wikipedia.org/wiki/Rec._709> 
+// But it also defines a transfer function.
+//
+// The electro-optical transfer function (EOTF) is the transfer
+// function having the picture or video signal as input and converting
+// it into the linear light output of the display. It is the inverse
+// of the OETF.
+//
+// The opto-electronic transfer function (OETF) is the transfer
+// function having the scene light as input and converting into the
+// picture or video signal as output. 
 
+// ----------------------------------------------------------------------  
+// TRANSFER FUNCTIONS FOLLOW
+// ----------------------------------------------------------------------  
+
+// Converts a color from non-linear R'G'B' to linear RGB encoding;
+// 
+// In case you are reading from an image format that is `*_SRGB`, this
+// function is automatically applied on texel read. The texel rgb
+// values are then assumed to be in linear sRGB colorspace.
+// 
+// This is the inverse of `inverse_eotf_srgb`.
+//
+vec3 eotf_srgb(in const vec3 non_linear_rgb) {
+    bvec3 cutoff = lessThan(non_linear_rgb, vec3(0.04045));
+    vec3 lower = non_linear_rgb/vec3(12.92);
+    vec3 higher = pow((non_linear_rgb + vec3(0.055))/vec3(1.055), vec3(2.4));
     return mix(higher, lower, cutoff);
 }
 
-// Converts a color from sRGB gamma to linear light gamma
-vec3 non_linear_srgb_to_linear_srgb(vec3 sRGB)
-{
-    bvec3 cutoff = lessThan(sRGB, vec3(0.04045));
-    vec3 higher = pow((sRGB + vec3(0.055))/vec3(1.055), vec3(2.4));
-    vec3 lower = sRGB/vec3(12.92);
-
+// Converts a color from linear RGB to non-linear R'G'B'. 
+//
+// This uses the correct OETF (opto-electrical transfer function)
+// based on the bt.709 standard:
+// <https://en.wikipedia.org/wiki/Rec._709>
+//
+// In case you are writing to an image format that is `*_SRGB`, this
+// function is automatically applied on texel save by the driver; you
+// don't have to apply it again.
+//
+// This is the inverse of `eotf_srgb`.
+//
+vec3 inverse_eotf_srgb(in const vec3 linear_rgb) { 
+	bvec3 cutoff = lessThan(linear_rgb, vec3(0.0031308));
+	vec3 lower = linear_rgb * vec3(12.92);
+	vec3 higher = vec3(1.055)*pow(linear_rgb, vec3(1.0/2.4)) - vec3(0.055);
     return mix(higher, lower, cutoff);
 }
