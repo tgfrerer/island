eeed16a87ee1baaeed015194447a54901e2a8763
tgfrerer <tim@poniesandlight.co.uk>
2026-01-29 10:19:44 +0000

    [le_swapchain_img] remove unused field

--- modules/le_swapchain_vk/le_swapchain_img.h
+++ modules/le_swapchain_vk/le_swapchain_img.h
@@ -16,7 +16,6 @@ struct le_swapchain_img_settings_t {
 	struct le_image_encoder_interface_t* image_encoder_i          = nullptr;                    // ffdecl. declared in shared/interfaces/le_image_encoder_interface.h
 	void*                                image_encoder_parameters = nullptr;                    // non-owning
 	char const*                          image_filename_template  = nullptr;                    // a format string, must contain %d for current image number.
-	char const*                          pipe_cmd                 = nullptr;                    // command used to save images - will receive stream of images via stdin
 	size_t                               frame_number_offset      = 0;                          // optional; used to calculate output frame number name -- this offset is added to the running frame number number
 
 	operator le_swapchain_settings_t*() {
