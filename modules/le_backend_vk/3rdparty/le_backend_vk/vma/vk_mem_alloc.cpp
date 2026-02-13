019f4e01250623d1095bccabd964ac7050c33611
tgfrerer <tim@poniesandlight.co.uk>
2026-02-13 23:02:29 +0000

    [backend] pacify clang compilation warnings for vk-mem-alloc

--- modules/le_backend_vk/3rdparty/le_backend_vk/vma/vk_mem_alloc.cpp
+++ modules/le_backend_vk/3rdparty/le_backend_vk/vma/vk_mem_alloc.cpp
@@ -1,3 +1,10 @@
 #define VMA_IMPLEMENTATION
 
+// tig: we are disabling these diagnostics for now, because they were just overspilling the logs
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnullability-completeness"
+
 #include "vk_mem_alloc.h"
+
+#pragma clang diagnostic pop
