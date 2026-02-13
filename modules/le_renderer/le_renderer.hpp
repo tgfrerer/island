ad2611393f55a7463376d8a8de550a7e2bf3d23d
tgfrerer <tim@poniesandlight.co.uk>
2026-02-13 17:53:12 +0000

    [renderer] warn if generated resources are not used after creation

--- modules/le_renderer/le_renderer.hpp
+++ modules/le_renderer/le_renderer.hpp
@@ -281,15 +281,15 @@ class Renderer : NoMove, NoCopy {
 		return le_renderer::renderer_i.get_swapchain_extent( self, swapchain, pWidth, pHeight );
 	}
 
-	le_image_resource_handle createImageResourceHandle( const char* name = nullptr, uint8_t num_samples = 0, le_image_resource_handle_t::UsageFlagBits flags = {} ) {
+	[[nodiscard]] le_image_resource_handle createImageResourceHandle( const char* name = nullptr, uint8_t num_samples = 0, le_image_resource_handle_t::UsageFlagBits flags = {} ) {
 		return le_renderer::renderer_i.create_img_resource_handle( self, name, num_samples, flags );
 	}
 
-	le_buffer_resource_handle createBufferResourceHandle( const char* name = nullptr, le_buffer_resource_handle_t::UsageFlags flags = {}, uint16_t index = 0 ) {
+	[[nodiscard]] le_buffer_resource_handle createBufferResourceHandle( const char* name = nullptr, le_buffer_resource_handle_t::UsageFlags flags = {}, uint16_t index = 0 ) {
 		return le_renderer::renderer_i.create_buf_resource_handle( self, name, flags, index );
 	}
 
-	le_texture_handle produceTextureHandle( const char* name = nullptr ) {
+	[[nodiscard]] le_texture_handle produceTextureHandle( const char* name = nullptr ) {
 		return le_renderer::renderer_i.produce_texture_handle( self, name );
 	}
 
