506a5cb7d2ea8ae91626ea4cf81ea241654d0b41
tgfrerer <tim@poniesandlight.co.uk>
2026-06-16 15:39:07 +0100

    [backend] correctly warn if stencil mode is selected
    - this can happen if a depth texture format is selected that uses a
    stencil component

--- modules/le_backend_vk/le_backend_vk.cpp
+++ modules/le_backend_vk/le_backend_vk.cpp
@@ -2618,11 +2618,13 @@ static void backend_create_rendering_attachment_infos( BackendFrameData& frame, 
 
 				if ( is_depth || is_stencil ) {
 					pass.depth_format = VkFormat( attachment->format );
-					if ( is_stencil && false == is_stencil_did_warn ) {
-						resolve_mode = VK_RESOLVE_MODE_MAX_BIT;
-						logger().warn( "Renderpass: '%s'", pass.debugName );
-						logger().warn( "Stencil testing is not implemented currently." );
-						is_stencil_did_warn = true; // we only want to warn once.
+					if ( is_stencil ) {
+						resolve_mode = VK_RESOLVE_MODE_NONE;
+						if ( false == is_stencil_did_warn ) {
+							logger().warn( "Renderpass: '%s'", pass.debugName );
+							logger().warn( "Stencil testing is not implemented currently." );
+							is_stencil_did_warn = true; // we only want to warn once.
+						}
 					}
 				} else {
 					pass.color_formats.push_back( VkFormat( attachment->format ) );
